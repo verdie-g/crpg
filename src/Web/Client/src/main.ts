78590f4fb9af6bf77bceb336a03a3fcec0cf4563
verdie-g <gregoire.verdier@gmail.com>
2020-02-08 18:33:04 +0100

    Add signin view

--- src/Web/Client/src/main.ts
+++ src/Web/Client/src/main.ts
@@ -1,7 +1,15 @@
 import Vue from 'vue';
+import Buefy from 'buefy';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'buefy/dist/buefy.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+
+Vue.use(Buefy, {
+  defaultIconPack: 'fas',
+});
 
 Vue.config.productionTip = false;
 
---
+++ src/Web/Client/src/views/SignIn.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="section">
+    <div class="container content is-large">
+      <h1>tRPG</h1>
+      <p>
+        Inspired by <a href="https://c-rpg.net" target="_blank">cRPG</a>, tRPG is a mod for
+        <a href="https://store.steampowered.com/app/261550/Mount__Blade_II_Bannerlord" target="_blank">Mount & Blade II: Bannerlord</a>.
+        It adds persistence to the multiplayer. Your start as a peasant and you'll develop your unique character with different stats and
+        equipments.
+      </p>
+      <b-button size="is-large" icon-left="steam-symbol" icon-pack="fab">
+        Sign in through Steam
+      </b-button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class SignIn extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+</style>
