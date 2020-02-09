bfc8665eecc49363cee60576d331fd0e6077ad85
verdie-g <gregoire.verdier@gmail.com>
2020-02-09 12:59:14 +0100

    webui: introduce auth utils

--- src/Web/Client/.eslintrc.js
+++ src/Web/Client/.eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'max-len': ['warn', { 'code': 140 }],
+    'max-len': ['warn', { code: 140 }],
+    'class-methods-use-this': 'off',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
