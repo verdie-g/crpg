646d515047cc8f6bd318ae680706b17eb7d80d99
verdie-g <gregoire.verdier@gmail.com>
2020-02-12 22:44:41 +0100

    webui: add navbar

--- src/Web/Client/src/store/user-module.ts
+++ src/Web/Client/src/store/user-module.ts
@@ -1,13 +1,11 @@
-import {
-  Module, VuexModule, Mutation, Action, getModule,
-} from 'vuex-module-decorators';
+import { Module, VuexModule, Mutation, Action, getModule } from 'vuex-module-decorators';
+import store from '@/store';
 import User from '@/models/user';
 import * as userService from '@/services/users-service';
-import store from './index';
 
 @Module({ store, dynamic: true, name: 'user' })
-export class UserModule extends VuexModule {
-  user: User;
+class UserModule extends VuexModule {
+  user: User | null = null;
 
   @Mutation
   setUser(user: User) {
