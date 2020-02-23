ea987a6f27dc4f02b5ae7173ef60065fbe68aad1
verdie-g <gregoire.verdier@gmail.com>
2020-02-23 13:37:03 +0100

    server: add persistence tests

---
+++ test/Persistence.UTest/TrpgDbContextTest.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using Trpg.Common;
+using Trpg.Domain.Entities;
+using Trpg.Persistence;
+
+namespace Persistence.UTest
+{
+    public class TrpgDbContextTest
+    {
+        [Test]
+        public async Task AuditableEntitySetCreatedAtOnCreation()
+        {
+            var options = new DbContextOptionsBuilder<TrpgDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var dt = new DateTime(2000, 01, 02);
+            var idt = new Mock<IDateTime>();
+            idt.SetupGet(i => i.Now).Returns(dt);
+            var db = new TrpgDbContext(options, idt.Object);
+
+            var character = new Character();
+            db.Add(character);
+            await db.SaveChangesAsync();
+
+            Assert.AreEqual(dt, character.LastModifiedAt);
+            Assert.AreEqual(dt, character.CreatedAt);
+        }
+
+        [Test]
+        public async Task AuditableEntitySetModifiedAtOnUpdate()
+        {
+            var options = new DbContextOptionsBuilder<TrpgDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var dt1 = new DateTime(2000, 01, 02);
+            var dt2 = new DateTime(2000, 01, 03);
+            var idt = new Mock<IDateTime>();
+            idt.SetupSequence(i => i.Now)
+               .Returns(dt1) // LastModifiedAt
+               .Returns(dt1) // CreatedAt
+               .Returns(dt2); // LastModifiedAt
+            var db = new TrpgDbContext(options, idt.Object);
+
+            var character = new Character();
+            db.Add(character);
+            await db.SaveChangesAsync();
+
+            character.Name = "toto";
+            await db.SaveChangesAsync();
+
+            Assert.AreEqual(dt2, character.LastModifiedAt);
+        }
+    }
+}
