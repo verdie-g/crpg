6dbe546bf5a9b7008a40a90328944565f965b08a
verdie-g <gregoire.verdier@gmail.com>
2020-02-24 22:01:16 +0100

    webapi: move constants to Constants.cs

---
+++ src/Application/Common/Constants.cs
@@ -0,0 +1,10 @@
+using Crpg.Domain.Entities;
+
+namespace Crpg.Application.Common
+{
+    public class Constants
+    {
+        public const int StartingGolds = 300;
+        public const Role DefaultRole = Role.User;
+    }
+}
