ae6ee6f0b937706b9bf09e036bd6dbd1af4d1340
Jānis Putniņš <janis.putnins@in-volv.com>
2020-02-29 16:16:19 +0200

    Use UTC time.

--- src/Infrastructure/MachineDateTime.cs
+++ src/Infrastructure/MachineDateTime.cs
@@ -5,6 +5,6 @@ namespace Crpg.Infrastructure
 {
     public class MachineDateTime : IDateTime
     {
-        public DateTime Now => DateTime.Now;
+        public DateTime Now => DateTime.UtcNow;
     }
 }
