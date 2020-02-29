c2406749ccfb04b5846f366ff965d1cdcaf2b817
Jānis Putniņš <janis.putnins@in-volv.com>
2020-02-29 16:37:59 +0200

    Revert "Use UTC time."
    This reverts commit ae6ee6f0b937706b9bf09e036bd6dbd1af4d1340.

--- src/Infrastructure/MachineDateTime.cs
+++ src/Infrastructure/MachineDateTime.cs
@@ -5,6 +5,6 @@ namespace Crpg.Infrastructure
 {
     public class MachineDateTime : IDateTime
     {
-        public DateTime Now => DateTime.UtcNow;
+        public DateTime Now => DateTime.Now;
     }
 }
