e77b21bc62a633f6ebca6631f3c4da91023c4192
verdie-g <gregoire.verdier@gmail.com>
2020-03-02 19:55:40 +0100

    webui: add isSignedIn in state (resolves #19)

--- src/Web/Client/src/services/auth-service.ts
+++ src/Web/Client/src/services/auth-service.ts
@@ -2,7 +2,7 @@ const TOKEN_KEY = 'token';
 const REDIRECT_URI = window.location.origin;
 const API_BASE_URI = 'http://localhost:5000/api';
 
-export function getToken(): string {
+export function getToken(): string | undefined {
   return localStorage[TOKEN_KEY];
 }
 
@@ -10,14 +10,10 @@ export function setToken(token: string) {
   localStorage[TOKEN_KEY] = token;
 }
 
-export function signIn() {
+export function challenge() {
   window.location.href = `${API_BASE_URI}/auth/signIn?redirectUri=${encodeURIComponent(REDIRECT_URI)}`;
 }
 
-export function signOut() {
+export function clearToken() {
   localStorage.removeItem(TOKEN_KEY);
-}
-
-export function isSignedIn(): boolean {
-  return getToken() !== undefined;
 }
