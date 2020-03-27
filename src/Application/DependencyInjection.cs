7bec0eb220dbf85b0cd06ec58404fb1dfeaf85d9
verdie-g <gregoire.verdier@gmail.com>
2020-03-27 23:24:51 +0100

    server: fix validators not injected

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using AutoMapper;
 using Crpg.Application.Common.Behaviors;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,10 +11,11 @@ namespace Crpg.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-            services.AddAutoMapper(Assembly.GetExecutingAssembly());
-            services.AddMediatR(Assembly.GetExecutingAssembly());
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestMetricBehavior<,>));
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+            services.AddAutoMapper(Assembly.GetExecutingAssembly())
+                .AddMediatR(Assembly.GetExecutingAssembly())
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestMetricBehavior<,>))
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>))
+                .AddValidatorsFromAssemblyContaining(typeof(DependencyInjection));
 
             return services;
         }
