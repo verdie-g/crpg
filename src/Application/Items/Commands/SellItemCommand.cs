ca12ad25b7e512799239d5b38032bba5f2eeda11
verdie-g <gregoire.verdier@gmail.com>
2020-03-30 22:51:35 +0200

    server: unequip item before selling it

--- src/Application/Items/Commands/SellItemCommand.cs
+++ src/Application/Items/Commands/SellItemCommand.cs
@@ -18,18 +18,16 @@ namespace Crpg.Application.Items.Commands
         public class Handler : IRequestHandler<SellItemCommand>
         {
             private readonly ICrpgDbContext _db;
-            private readonly IMapper _mapper;
 
-            public Handler(ICrpgDbContext db, IMapper mapper)
+            public Handler(ICrpgDbContext db)
             {
                 _db = db;
-                _mapper = mapper;
             }
 
             public async Task<Unit> Handle(SellItemCommand request, CancellationToken cancellationToken)
             {
                 var userItem = await _db.UserItems
-                    .Include(ui => ui.User)
+                    .Include(ui => ui.User).ThenInclude(u => u!.Characters)
                     .Include(ui => ui.Item)
                     .FirstOrDefaultAsync(ui => ui.UserId == request.UserId && ui.ItemId == request.ItemId, cancellationToken);
 
@@ -40,8 +38,61 @@ namespace Crpg.Application.Items.Commands
 
                 userItem.User!.Gold += (int)(userItem.Item!.Value * Constants.SellItemRatio);
                 _db.UserItems.Remove(userItem);
+                foreach (var character in userItem.User.Characters)
+                {
+                    UnsetItem(character, userItem.ItemId);
+                }
+
                 await _db.SaveChangesAsync(cancellationToken);
                 return Unit.Value;
+            }
+
+            private static void UnsetItem(Character character, int itemId)
+            {
+                if (character.HeadItemId == itemId)
+                {
+                    character.HeadItemId = null;
+                }
+                else if (character.CapeItemId == itemId)
+                {
+                    character.CapeItemId = null;
+                }
+                else if (character.BodyItemId == itemId)
+                {
+                    character.BodyItemId = null;
+                }
+                else if (character.HandItemId == itemId)
+                {
+                    character.HandItemId = null;
+                }
+                else if (character.LegItemId == itemId)
+                {
+                    character.LegItemId = null;
+                }
+                else if (character.HorseHarnessItemId == itemId)
+                {
+                    character.HorseHarnessItemId = null;
+                }
+                else if (character.HorseItemId == itemId)
+                {
+                    character.HorseItemId = null;
+                }
+                else if (character.Weapon1ItemId == itemId)
+                {
+                    character.Weapon1ItemId = null;
+                }
+                else if (character.Weapon2ItemId == itemId)
+                {
+                    character.Weapon2ItemId = null;
+                }
+                else if (character.Weapon3ItemId == itemId)
+                {
+                    character.Weapon3ItemId = null;
+                }
+                else if (character.Weapon4ItemId == itemId)
+                {
+                    character.Weapon4ItemId = null;
+                }
             }
         }
     }
