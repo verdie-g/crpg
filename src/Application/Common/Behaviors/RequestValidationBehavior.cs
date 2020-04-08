a3f375728570a62db02c53271428b6ff28d35d4e
verdie-g <gregoire.verdier@gmail.com>
2020-04-08 18:21:12 +0200

    server: skip validation if there is no validators

--- src/Application/Common/Behaviors/RequestValidationBehavior.cs
+++ src/Application/Common/Behaviors/RequestValidationBehavior.cs
@@ -10,16 +10,21 @@ namespace Crpg.Application.Common.Behaviors
     public class RequestValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
             where TRequest : IRequest<TResponse>
     {
-        private readonly IEnumerable<IValidator<TRequest>> _validators;
+        private readonly IValidator<TRequest>[] _validators;
 
         public RequestValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
         {
-            _validators = validators;
+            _validators = (validators as IValidator<TRequest>[])!;
         }
 
         public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
             RequestHandlerDelegate<TResponse> next)
         {
+            if (_validators.Length == 0)
+            {
+                return next();
+            }
+
             var context = new ValidationContext(request);
 
             var failures = _validators
