51f20f38b4137adf5ed492167b0a912b4287e31b
verdie-g <gregoire.verdier@gmail.com>
2020-04-17 22:57:16 +0200

    game: add client for crpg api

---
+++ src/GameMod/Api/Requests/GetUserRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Crpg.GameMod.Api.Requests
+{
+    /// <summary>
+    /// Copy of Crpg.Application.Games.Commands.UpsertGameUserCommand.
+    /// </summary>
+    public class GetUserRequest
+    {
+        public long SteamId { get; set; }
+        public string CharacterName { get; set; } = default!;
+    }
+}
---
+++ src/GameMod/Api/Requests/TickRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Crpg.GameMod.Api.Requests
+{
+    /// <summary>
+    /// Copy of Crpg.Application.Games.Commands.TickCommand.
+    /// </summary>
+    public class TickRequest
+    {
+        public IReadOnlyList<UserTick> Users { get; set; } = Array.Empty<UserTick>();
+    }
+
+    /// <summary>
+    /// Copy of Crpg.Application.Games.Models.UserTick.
+    /// </summary>
+    public class UserTick
+    {
+        public int CharacterId { get; set; }
+        public int ExperienceGain { get; set; }
+        public int GoldGain { get; set; }
+    }
+}
