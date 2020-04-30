2d96417e3ba515b40872d659e7479b1edb638f5c
verdie-g <gregoire.verdier@gmail.com>
2020-04-30 22:28:09 +0200

    game: document skirmish mission behaviours

--- src/GameMod/Battle/CrpgBattleGameMode.cs
+++ src/GameMod/Battle/CrpgBattleGameMode.cs
@@ -19,13 +19,13 @@ namespace Crpg.GameMod.Battle
         {
             DebugUtils.Trace(scene);
 
+            // inspired by TaleWorlds.MountAndBlade.MultiplayerMissions
             MissionState.OpenNew("MultiplayerSkirmish", new MissionInitializerRecord(scene), InitializeMissionBehaviours,
                 true, true, false);
         }
 
         private IEnumerable<MissionBehaviour> InitializeMissionBehaviours(Mission mission)
         {
-            // inspired by TaleWorlds.MountAndBlade.MultiplayerMissions
             return GameNetwork.IsServer
                 ? InitializeMissionBehavioursServer()
                 : InitializeMissionBehavioursClient();
@@ -41,25 +41,27 @@ namespace Crpg.GameMod.Battle
 
             return new MissionBehaviour[]
             {
-                MissionLobbyComponent.CreateBehaviour(),
-                new MultiplayerRoundController(),
-                new MissionMultiplayerFlagDomination(true),
-                new MultiplayerWarmupComponent(),
+                MissionLobbyComponent.CreateBehaviour(), // ???
+                new MultiplayerRoundController(), // starts/stops round, ends match
+                new MissionMultiplayerFlagDomination(true), // flag + morale logic
+                new MultiplayerWarmupComponent(), // warmup logic
                 new MissionMultiplayerGameModeFlagDominationClient(),
-                new MultiplayerTimerComponent(),
-                new MultiplayerMissionAgentVisualSpawnComponent(),
+                new MultiplayerTimerComponent(), // round timer
+                new MultiplayerMissionAgentVisualSpawnComponent(), // expose method to spawn an agent
                 new SpawnComponent(new FlagDominationSpawnFrameBehaviour(), spawningBehaviour),
-                new MissionLobbyEquipmentNetworkComponent(),
-                new MultiplayerTeamSelectComponent(),
-                new AgentVictoryLogic(),
-                new AgentBattleAILogic(),
-                new MissionBoundaryPlacer(),
-                new MissionBoundaryCrossingHandler(),
-                new MultiplayerPollComponent(),
-                new MultiplayerAdminComponent(),
+                new MissionLobbyEquipmentNetworkComponent(), // logic to change troop or perks
+                new MultiplayerTeamSelectComponent(), // logic to change team, autoselect
+                new AgentVictoryLogic(), // AI cheering when winning round
+                new AgentBattleAILogic(), // bot intelligence
+                new MissionBoundaryPlacer(), // set walkable boundaries
+                new MissionBoundaryCrossingHandler(), // kills agent out of mission boundaries
+                new MultiplayerPollComponent(), // poll logic to kick player, ban player, change game
+                new MultiplayerAdminComponent(), // admin UI to kick player or restart game
+                // used to send notifications (e.g. flag captured, round won) to peer. same logic can be used to send gold + xp gains
                 new MultiplayerGameNotificationsComponent(),
-                new MissionOptionsComponent(),
-                new MissionScoreboardComponent("Skirmish")
+                new MissionOptionsComponent(), // ???
+                // score board, parameter is used to get the right IScoreboardData implementation for the game mode
+                new MissionScoreboardComponent("Skirmish"),
             };
         }
 
@@ -75,13 +77,13 @@ namespace Crpg.GameMod.Battle
                 new MultiplayerMissionAgentVisualSpawnComponent(),
                 new MissionLobbyEquipmentNetworkComponent(),
                 new MultiplayerTeamSelectComponent(),
-                new AgentVictoryLogic(),
+                new AgentVictoryLogic(), // AI cheering when winning round
                 new MissionBoundaryPlacer(),
                 new MissionBoundaryCrossingHandler(),
                 new MultiplayerPollComponent(),
                 new MultiplayerGameNotificationsComponent(),
                 new MissionOptionsComponent(),
-                new MissionScoreboardComponent("Skirmish")
+                new MissionScoreboardComponent("Skirmish"),
             };
         }
     }
