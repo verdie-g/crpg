9230f3079ec3fe95a080f894eae956c2ecf08243
verdie-g <gregoire.verdier@gmail.com>
2020-05-06 15:47:57 +0200

    webui: reset stats delta when selectin character

--- src/WebUI/src/views/Characters.vue
+++ src/WebUI/src/views/Characters.vue
@@ -12,99 +12,9 @@
       </div>
 
       <div class="column">
-        <div class="columns container is-fluid" v-if="selectedCharacter">
-
-          <character-stats :character="selectedCharacter" class="column character-stats" />
-
-          <div class="column character-items">
-            <div class="columns item-boxes">
-              <div class="column is-narrow gear-column">
-                <div class="box item-box" @click="openReplaceItemModal(itemSlot.Head)">
-                  <img v-if="selectedCharacter.items.headItem" :src="selectedCharacter.items.headItem.image" alt="Head armor" />
-                  <img v-else src="../assets/head-armor.png" alt="Head armor" class="item-placeholder" />
-                </div>
-                <div class="box item-box" @click="openReplaceItemModal(itemSlot.Cape)">
-                  <img v-if="selectedCharacter.items.capeItem" :src="selectedCharacter.items.capeItem.image" alt="Cape" />
-                  <img v-else src="../assets/cape.png" alt="Cape" class="item-placeholder" />
-                </div>
-                <div class="box item-box" @click="openReplaceItemModal(itemSlot.Body)">
-                  <img v-if="selectedCharacter.items.bodyItem" :src="selectedCharacter.items.bodyItem.image" alt="Body armor" />
-                  <img v-else src="../assets/body-armor.png" alt="Body armor" class="item-placeholder" />
-                </div>
-                <div class="box item-box" @click="openReplaceItemModal(itemSlot.Hand)">
-                  <img v-if="selectedCharacter.items.handItem" :src="selectedCharacter.items.handItem.image" alt="Hand armor" />
-                  <img v-else src="../assets/hand-armor.png" alt="Hand armor" class="item-placeholder" />
-                </div>
-                <div class="box item-box" @click="openReplaceItemModal(itemSlot.Leg)">
-                  <img v-if="selectedCharacter.items.legItem" :src="selectedCharacter.items.legItem.image" alt="Leg armor" />
-                  <img v-else src="../assets/leg-armor.png" alt="Leg armor" class="item-placeholder" />
-                </div>
-              </div>
-
-              <div class="column is-narrow horse-column">
-                <div class="box item-box" @click="openReplaceItemModal(itemSlot.HorseHarness)">
-                  <img v-if="selectedCharacter.items.horseHarnessItem" :src="selectedCharacter.items.horseHarnessItem.image" alt="Horse harness" />
-                  <img v-else src="../assets/horse-harness.png" alt="Horse harness" class="item-placeholder" />
-                </div>
-                <div class="box item-box" @click="openReplaceItemModal(itemSlot.Horse)">
-                  <img v-if="selectedCharacter.items.horseItem" :src="selectedCharacter.items.horseItem.image" alt="Horse" />
-                </div>
-              </div>
-
-              <div class="column is-narrow weapon-column">
-                <div class="box item-box" @click="openReplaceItemModal(itemSlot.Weapon1)">
-                  <img v-if="selectedCharacter.items.weapon1Item" :src="selectedCharacter.items.weapon1Item.image" alt="First weapon" />
-                </div>
-                <div class="box item-box" @click="openReplaceItemModal(itemSlot.Weapon2)">
-                  <img v-if="selectedCharacter.items.weapon2Item" :src="selectedCharacter.items.weapon2Item.image" alt="Second weapon" />
-                </div>
-                <div class="box item-box" @click="openReplaceItemModal(itemSlot.Weapon3)">
-                  <img v-if="selectedCharacter.items.weapon3Item" :src="selectedCharacter.items.weapon3Item.image" alt="Third weapon" />
-                </div>
-                <div class="box item-box" @click="openReplaceItemModal(itemSlot.Weapon4)">
-                  <img v-if="selectedCharacter.items.weapon4Item" :src="selectedCharacter.items.weapon4Item.image" alt="Fourth Weapon" />
-                </div>
-              </div>
-            </div>
-
-            <b-button type="is-warning" icon-left="angle-double-down" expanded disabled>Respecialize</b-button>
-            <b-button type="is-warning" icon-left="baby"
-                      expanded :disabled="selectedCharacter.level < 31"
-                      @click="openRetireCharacterDialog">Retire</b-button>
-            <b-button type="is-danger" icon-left="trash" expanded @click="openDeleteCharacterDialog">Delete</b-button>
-          </div>
-
-          <b-modal :active.sync="isReplaceItemModalActive" scroll="keep">
-            <div class="columns is-marginless replace-item-modal">
-              <div class="column" v-if="itemToReplace">
-                <h3>Replace <strong>{{itemToReplace.name}}</strong></h3>
-                <item-properties :item="itemToReplace" />
-              </div>
-              <div class="column owned-items">
-                <div v-if="fittingOwnedItems.length" class="columns is-multiline">
-                  <div class="column is-narrow owned-item" v-for="ownedItem in fittingOwnedItems"
-                       v-bind:key="ownedItem.id" @click="selectedItem = ownedItem">
-                    <figure class="image">
-                      <img :src="ownedItem.image" alt="item image" />
-                    </figure>
-                    <h4>{{ownedItem.name}}</h4>
-                    <item-properties :item="ownedItem" />
-                  </div>
-                </div>
-                <div v-else>You don't own any item for this type.</div>
-              </div>
-              <div class="column" v-if="selectedItem">
-                <h3>Replace with <strong>{{selectedItem.name}}</strong></h3>
-                <div class="content">
-                  <item-properties :item="selectedItem" />
-                  <b-button size="is-medium" icon-left="check" expanded @click="confirmItemSelection" />
-                </div>
-              </div>
-            </div>
-          </b-modal>
-
+        <div v-if="selectedCharacter">
+          <character-component :character="selectedCharacter"  />
         </div>
-
         <div v-else> <!-- if no character -->
           To create a character, simply connect to one of the cRPG servers.
         </div>
@@ -115,155 +25,35 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import ItemProperties from '@/components/ItemProperties.vue';
 import userModule from '@/store/user-module';
 import Character from '@/models/character';
-import ItemSlot from '@/models/item-slot';
-import Item from '@/models/item';
-import { getCharacterItemFromSlot } from '@/services/characters-service';
-import { filterItemsFittingInSlot } from '@/services/item-service';
-import { notify } from '@/services/notifications-service';
-import CharacterStats from '@/components/CharacterStats.vue';
+import CharacterComponent from '@/components/CharacterComponent.vue';
 
 @Component({
-  components: { CharacterStats, ItemProperties },
+  components: { CharacterComponent },
 })
-export default class Characters extends Vue {
-    selectedCharacterId: number = -1;
+export default class CharactersComponent extends Vue {
+  selectedCharacterId: number = -1;
 
-    // modal stuff
-    itemSlot = ItemSlot;
-    isReplaceItemModalActive: boolean = false;
-    itemToReplace: Item | null = null;
-    itemToReplaceSlot: ItemSlot | null = null;
-    selectedItem: Item | null = null;
+  get characters() {
+    return userModule.characters;
+  }
 
-    get characters() {
-      return userModule.characters;
-    }
+  get selectedCharacter(): Character | null {
+    return this.selectedCharacterId === -1
+      ? null
+      : this.characters.find(c => c.id === this.selectedCharacterId)!;
+  }
 
-    get selectedCharacter(): Character | null {
-      return this.selectedCharacterId === -1
-        ? null
-        : this.characters.find(c => c.id === this.selectedCharacterId)!;
-    }
+  created() {
+    userModule.getCharacters().then(c => this.selectedCharacterId = c.length > 0 ? c[0].id : -1);
+  }
 
-    get fittingOwnedItems() : Item[] {
-      return this.itemToReplaceSlot === null
-        ? []
-        : filterItemsFittingInSlot(userModule.ownedItems, this.itemToReplaceSlot)
-          .filter(i => this.itemToReplace === null || i.id !== this.itemToReplace.id);
-    }
-
-    created() {
-      userModule.getCharacters().then(c => this.selectedCharacterId = c.length > 0 ? c[0].id : -1);
-    }
-
-    selectCharacter(character: Character) {
-      this.selectedCharacterId = character.id;
-    }
-
-    openRetireCharacterDialog() {
-      this.$buefy.dialog.confirm({
-        title: 'Retiring character',
-        message: `Are you sure you want to retire your character ${this.selectedCharacter!.name} lvl. ${this.selectedCharacter!.level}? This action cannot be undone.`,
-        confirmText: 'Retire',
-        type: 'is-warning',
-        hasIcon: true,
-        onConfirm: () => {
-          userModule.retireCharacter(this.selectedCharacter!);
-          notify('Character retired');
-        },
-      });
-    }
-
-    openDeleteCharacterDialog() {
-      this.$buefy.dialog.confirm({
-        title: 'Deleting character',
-        message: `Are you sure you want to delete your character ${this.selectedCharacter!.name} lvl. ${this.selectedCharacter!.level}? This action cannot be undone.`,
-        confirmText: 'Delete Character',
-        type: 'is-danger',
-        hasIcon: true,
-        onConfirm: () => {
-          userModule.deleteCharacter(this.selectedCharacter!);
-          notify('Character deleted');
-        },
-      });
-    }
-
-    openReplaceItemModal(slot: ItemSlot) {
-      this.itemToReplace = getCharacterItemFromSlot(this.selectedCharacter!.items, slot);
-      this.itemToReplaceSlot = slot;
-      this.selectedItem = null;
-      if (userModule.ownedItems.length === 0) {
-        userModule.getOwnedItems();
-      }
-
-      this.isReplaceItemModalActive = true;
-    }
-
-    confirmItemSelection() {
-      userModule.replaceItem({ character: this.selectedCharacter!, slot: this.itemToReplaceSlot!, item: this.selectedItem! });
-      this.isReplaceItemModalActive = false;
-    }
+  selectCharacter(character: Character) {
+    this.selectedCharacterId = character.id;
+  }
 }
 </script>
 
 <style scoped lang="scss">
-  .item-boxes {
-    background-image: url("../assets/body-silhouette.svg");
-    background-repeat: no-repeat;
-    background-size: 190px;
-    background-position: 180px 0px;
-    padding-bottom: 8px; // so the silhouette's feet ain't cropped
-  }
-
-  .horse-column {
-    width: 250px;
-    display: flex;
-    justify-content: flex-end;
-    flex-direction: column;
-    align-items: center;
-  }
-
-  .item-box {
-    // item image dimensions / 2
-    width: 128px;
-    height: 60px;
-    padding: 0;
-    cursor: pointer;
-    text-align: center; // to align horizontally placeholder
-
-    &:hover {
-      box-shadow: 0 5px 8px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
-    }
-
-    .item-placeholder {
-      margin-top: -3px; // because placeholder is 66 px in a 60px box
-      opacity: 0.3;
-    }
-  }
-
-  .replace-item-modal {
-    background-color: #fff; // TODO: replace with bulma variable
-    // inherit modal height
-    max-height: inherit;
-  }
-
-  .owned-items {
-    overflow: auto;
-
-    & > .columns {
-      justify-content: center;
-    }
-  }
-
-  .owned-item {
-    width: 256px;
-    cursor: pointer;
-
-    &:hover {
-      background-color: #fafafa; // TODO: use bulma variable
-    }
-  }
 </style>
