da107edb992dd7d56aa6b7d768d2caa2d19ac32d
verdie-g <gregoire.verdier@gmail.com>
2020-05-09 19:34:12 +0200

    webapi: use old cRPG experience table (resolves #5)

--- src/Application/Games/ExperienceTable.cs
+++ src/Application/Games/ExperienceTable.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace Crpg.Application.Games
 {
@@ -6,52 +7,21 @@ namespace Crpg.Application.Games
     {
         private const int TableStartLevel = 1;
 
-        // TODO: get real values from old crpg
-        private static readonly int[] Table =
-        {
-            0, // level 1
-            600, // level 2
-            1_360, // level 3
-            2_296, // ...
-            3_426,
-            4_768,
-            6_345,
-            8_179,
-            10_297,
-            13_010,
-            16_161,
-            19_806,
-            24_007,
-            28_832,
-            34_362,
-            40_682,
-            47_892,
-            56_103,
-            65_441,
-            77_233,
-            90_809,
-            106_425,
-            124_371,
-            144_981,
-            168_636,
-            195_769,
-            226_879,
-            262_533,
-            303_381,
-            350_164,
-            412_091,
-            484_440,
-            568_974,
-            667_638,
-            782_877,
-            917_424,
-            1_074_494,
-            1_257_843,
-            1_471_851,
-            1_721_626,
-            2_070_551,
-            2_489_361,
-        };
+        internal static readonly int[] Table =
+            Enumerable.Range(TableStartLevel, 35)
+                .Select(lvl =>
+                {
+                    // TODO: make curve steeper for lvl > 30
+                    if (lvl == 1)
+                    {
+                        return 0;
+                    }
+
+                    const int a = 3936;
+                    const int b = 13750;
+                    const int c = -30750;
+                    return (int)(a * Math.Pow(1.26, lvl) + b * lvl + c);
+                }).ToArray();
 
         /// <summary>
         /// Get the level for the total experience gained.
