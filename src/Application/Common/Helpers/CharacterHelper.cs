201f24ba8490a5df7793147a0317680a0a900cf7
verdie-g <gregoire.verdier@gmail.com>
2020-05-09 21:08:12 +0200

    webapi: reset character stats when retiring

--- src/Application/Common/Helpers/CharacterHelper.cs
+++ src/Application/Common/Helpers/CharacterHelper.cs
@@ -9,14 +9,41 @@ namespace Crpg.Application.Common.Helpers
         private const float DefaultExperienceMultiplier = 1.0f;
         private const int DefaultStrength = 3;
         private const int DefaultAgility = 3;
+        private const int DefaultWeaponProficienciesPoint = 57;
 
-        public static void SetDefaultValuesForNewCharacter(Character character)
+        public static void SetDefaultValuesForCharacter(Character character)
         {
             character.Level = DefaultLevel;
             character.Experience = DefaultExperience;
             character.ExperienceMultiplier = DefaultExperienceMultiplier;
-            character.Statistics.Attributes.Strength = DefaultStrength;
-            character.Statistics.Attributes.Agility = DefaultAgility;
+
+            character.Statistics = new CharacterStatistics
+            {
+                Attributes = new CharacterAttributes
+                {
+                    Strength = DefaultStrength,
+                    Agility = DefaultAgility,
+                },
+                WeaponProficiencies = new CharacterWeaponProficiencies
+                {
+                    Points = DefaultWeaponProficienciesPoint,
+                }
+            };
+        }
+
+        public static void UnequipCharacterItems(CharacterItems items)
+        {
+            items.HeadItemId = null;
+            items.CapeItemId = null;
+            items.BodyItemId = null;
+            items.HandItemId = null;
+            items.LegItemId = null;
+            items.HorseHarnessItem = null;
+            items.HorseItem = null;
+            items.Weapon1Item = null;
+            items.Weapon2Item = null;
+            items.Weapon3Item = null;
+            items.Weapon4Item = null;
         }
     }
 }
--- src/Application/Users/Commands/UpsertUserCommand.cs
+++ src/Application/Users/Commands/UpsertUserCommand.cs
@@ -66,7 +66,7 @@ namespace Crpg.Application.Users.Commands
 
                 if (_db.Entry(user).State == EntityState.Detached)
                 {
-                    UserHelper.SetDefaultValuesForNewUser(user);
+                    UserHelper.SetDefaultValuesForUser(user);
                     _db.Users.Add(user);
                     _events.Raise(EventLevel.Info, $"{request.UserName} joined ({request.SteamId})", string.Empty, "new_user");
                 }
