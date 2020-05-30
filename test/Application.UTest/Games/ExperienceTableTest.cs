261bc25f559af31839a3870a6c275e1962789113
verdie-g <gregoire.verdier@gmail.com>
2020-05-30 10:43:21 +0200

    webapi: round-up first level experience

--- test/Application.UTest/Games/ExperienceTableTest.cs
+++ test/Application.UTest/Games/ExperienceTableTest.cs
@@ -5,10 +5,13 @@ namespace Crpg.Application.UTest.Games
 {
     public class ExperienceTableTest
     {
+        // lvl 1: 3000
+        // lvl 2: 18375
+
         [TestCase(0, 1)]
-        [TestCase(2997, 1)]
-        [TestCase(2998, 2)]
+        [TestCase(2999, 1)]
         [TestCase(3000, 2)]
+        [TestCase(3001, 2)]
         [TestCase(20000, 3)]
         public void GetLevelForExperience(int experience, int expectedLevel)
         {
@@ -16,8 +19,8 @@ namespace Crpg.Application.UTest.Games
         }
 
         [TestCase(1, 0)]
-        [TestCase(2, 2998)]
-        [TestCase(3, 18373)]
+        [TestCase(2, 3000)]
+        [TestCase(3, 18375)]
         public void GetExperienceForLevel(int level, int expectedExperience)
         {
             Assert.AreEqual(expectedExperience, ExperienceTable.GetExperienceForLevel(level));
