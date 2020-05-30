5fb0524f9dcc8a74bb68a11d4a0053e54d8bc8ff
verdie-g <gregoire.verdier@gmail.com>
2020-05-30 11:56:59 +0200

    webapi: add skipped the fun boolean on character for later use (#13)

--- src/Domain/Entities/Character.cs
+++ src/Domain/Entities/Character.cs
@@ -10,6 +10,7 @@ namespace Crpg.Domain.Entities
         public int Experience { get; set; }
         public int Level { get; set; }
         public float ExperienceMultiplier { get; set; }
+        public bool SkippedTheFun { get; set; } // unused (issue #13)
         public CharacterStatistics Statistics { get; set; } = new CharacterStatistics();
         public CharacterItems Items { get; set; } = new CharacterItems();
 
