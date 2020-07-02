c98f88d70b1210f5c7f0a509dd8863459a799a3a
verdie-g <gregoire.verdier@gmail.com>
2020-07-02 19:05:42 +0200

    webapi: fix mapping

--- src/Application/Items/Models/ItemViewModel.cs
+++ src/Application/Items/Models/ItemViewModel.cs
@@ -1,12 +1,23 @@
+using System;
+using System.Collections.Generic;
 using AutoMapper;
 using Crpg.Application.Common.Mappings;
 using Crpg.Domain.Entities;
 
 namespace Crpg.Application.Items.Models
 {
-    public class ItemViewModel : ItemCreation, IMapFrom<Item>
+    public class ItemViewModel : IMapFrom<Item>
     {
         public int Id { get; set; }
+        public string MbId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public int Value { get; set; }
+        public ItemType Type { get; set; }
+        public float Weight { get; set; }
+
+        public ItemArmorComponentViewModel? Armor { get; set; }
+        public ItemHorseComponentViewModel? Horse { get; set; }
+        public IList<ItemWeaponComponentViewModel> Weapons { get; set; } = Array.Empty<ItemWeaponComponentViewModel>();
 
         public void Mapping(Profile profile)
         {
