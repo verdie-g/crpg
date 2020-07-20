fb9068ae45f7ac147bcace9dcf49f44a6ca70f31
verdie-g <gregoire.verdier@gmail.com>
2020-07-20 20:53:18 +0200

    webapi: add tracing to business layer

---
+++ src/Application/Common/Interfaces/Tracing/ITracer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Crpg.Application.Common.Interfaces.Tracing
+{
+    public interface ITracer
+    {
+        ITraceSpan CreateSpan(string name, IEnumerable<KeyValuePair<string, string>> tags);
+    }
+}
---
+++ src/Infrastructure/Tracing/Debug/DebugTracer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Crpg.Application.Common.Interfaces.Tracing;
+using Microsoft.Extensions.Logging;
+
+namespace Crpg.Infrastructure.Tracing.Debug
+{
+    internal class DebugTracer : ITracer
+    {
+        private readonly ILogger<DebugTracer> _logger;
+
+        public DebugTracer(ILogger<DebugTracer> logger) => _logger = logger;
+
+        public ITraceSpan CreateSpan(string name, IEnumerable<KeyValuePair<string, string>> tags)
+        {
+            var span = new DebugTraceSpan(name, tags, _logger);
+            _logger.Log(LogLevel.Debug, "Start of trace {0}", span.ToString());
+            return span;
+        }
+    }
+}
