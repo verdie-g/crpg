9563da0e6af53110da2c1f67658c563d81522606
verdie-g <gregoire.verdier@gmail.com>
2020-07-21 17:25:06 +0200

    webui: disable prefetch

--- src/WebUI/vue.config.js
+++ src/WebUI/vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   lintOnSave: 'warning',
+  // disable prefetch of chunks (usually pages) to only load required chunks for the current page
+  // https://medium.com/@mrodal/how-to-make-lazy-loading-actually-work-in-vue-cli-3-7f3f88cfb102
+  chainWebpack: config => config.plugins.delete('prefetch'),
 };
