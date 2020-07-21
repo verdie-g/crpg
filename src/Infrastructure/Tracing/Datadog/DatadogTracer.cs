75cc7946bfdaffbf80c0fc1e168b5e3744e9b163
verdie-g <gregoire.verdier@gmail.com>
2020-07-21 17:05:16 +0200

    webapi: add namespace to traces

--- src/Infrastructure/Tracing/Datadog/DatadogTracer.cs
+++ src/Infrastructure/Tracing/Datadog/DatadogTracer.cs
@@ -6,9 +6,16 @@ namespace Crpg.Infrastructure.Tracing.Datadog
 {
     internal class DatadogTracer : ITracer
     {
+        private readonly string _namespace;
+
+        public DatadogTracer(string ns)
+        {
+            _namespace = ns;
+        }
+
         public ITraceSpan CreateSpan(string name, IEnumerable<KeyValuePair<string, string>>? tags = null)
         {
-            Scope scope = Tracer.Instance.StartActive(name);
+            Scope scope = Tracer.Instance.StartActive(_namespace + "." + name);
             if (tags != null)
             {
                 foreach (var (key, value) in tags)
