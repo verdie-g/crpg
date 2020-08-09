ee064f6b3009c7ffe5e7a94b877885b5cecafe55
verdie-g <gregoire.verdier@gmail.com>
2020-08-09 17:33:47 +0200

    webapi: fix UpdateGameCommand endpoint return type

--- src/WebApi/Controllers/GamesController.cs
+++ src/WebApi/Controllers/GamesController.cs
@@ -13,7 +13,7 @@ namespace Crpg.WebApi.Controllers
         /// All-in-One endpoint to get or create users with character, give gold and experience, and break/repair items.
         /// </summary>
         [HttpPut("update")]
-        public async Task<ActionResult<GameUser>> Update([FromBody] UpdateGameCommand cmd)
+        public async Task<ActionResult<UpdateGameResult>> Update([FromBody] UpdateGameCommand cmd)
         {
             return Ok(await Mediator.Send(cmd));
         }
