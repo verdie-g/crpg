e4776df1d6b4df5af9ab99b33b2063bd6cf0b712
verdie-g <gregoire.verdier@gmail.com>
2020-09-26 16:56:57 +0200

    webapi: simplify migration creation code

--- src/Persistence/DesignTimeDbContextFactoryBase.cs
+++ src/Persistence/DesignTimeDbContextFactoryBase.cs
@@ -1,52 +1,20 @@
-using System;
-using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
 
 namespace Crpg.Persistence
 {
     public abstract class DesignTimeDbContextFactoryBase<TContext> : IDesignTimeDbContextFactory<TContext>
         where TContext : DbContext
     {
-        private const string ConnectionStringName = "Crpg";
-        private const string AspNetCoreEnvironment = "ASPNETCORE_ENVIRONMENT";
-
         public TContext CreateDbContext(string[] args)
         {
-            var basePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "WebApi"));
-            return Create(basePath, Environment.GetEnvironmentVariable(AspNetCoreEnvironment)
-                ?? throw new InvalidOperationException($"{AspNetCoreEnvironment} variable is not set"));
+            var options = new DbContextOptionsBuilder<TContext>()
+                .UseNpgsql("Database=crpg")
+                .UseSnakeCaseNamingConvention()
+                .Options;
+            return CreateNewInstance(options);
         }
 
         protected abstract TContext CreateNewInstance(DbContextOptions<TContext> options);
-
-        private TContext Create(string basePath, string environment)
-        {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(basePath)
-                .AddJsonFile($"appsettings.{environment}.json")
-                .AddEnvironmentVariables()
-                .Build();
-
-            var connectionString = configuration.GetConnectionString(ConnectionStringName);
-
-            return Create(connectionString);
-        }
-
-        private TContext Create(string connectionString)
-        {
-            if (string.IsNullOrEmpty(connectionString))
-            {
-                throw new ArgumentException($"Connection string '{ConnectionStringName}' is null or empty.",
-                    nameof(connectionString));
-            }
-
-            var optionsBuilder = new DbContextOptionsBuilder<TContext>();
-            optionsBuilder
-                .UseNpgsql(connectionString)
-                .UseSnakeCaseNamingConvention();
-            return CreateNewInstance(optionsBuilder.Options);
-        }
     }
 }
--- src/Persistence/README.md
+++ src/Persistence/README.md
@@ -1,6 +1,3 @@
 # Migrations
 
-First set environment variable ASPNETCORE\_ENVIRONMENT. Then create a migration
 ```dotnet ef migrations add MIGRATION_NAME```
-and update the database
-```dotnet ef database update```
