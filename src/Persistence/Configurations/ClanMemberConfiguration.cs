b56002f2ccce0a28aeb513de9972867f0a0f308d
verdie-g <gregoire.verdier@gmail.com>
2021-01-20 20:50:42 +0100

    Add clan member configuration

---
+++ src/Persistence/Configurations/ClanMemberConfiguration.cs
@@ -0,0 +1,14 @@
+using Crpg.Domain.Entities.Clans;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Crpg.Persistence.Configurations
+{
+    public class ClanMemberConfiguration : IEntityTypeConfiguration<ClanMember>
+    {
+        public void Configure(EntityTypeBuilder<ClanMember> builder)
+        {
+            builder.HasKey(cm => cm.UserId);
+        }
+    }
+}
