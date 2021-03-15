5cccc80354fcac5404348998fa31c7e0c31bf603
Falcomfr <falcominien@yahoo.fr>
2021-03-15 23:05:30 +0100

    Try to filter settlements displayed

--- src/WebUI/src/views/Strategus.vue
+++ src/WebUI/src/views/Strategus.vue
@@ -2,17 +2,21 @@
   <div>
     <l-map
       class="map"
+      ref="map"
       :zoom="zoom"
       :center="center"
       :options="mapOptions"
       :max-bounds="maxBounds"
       @click="infoPos"
+      @moveend="setVisibleBounds($refs.map.mapObject.getBounds())"
     >
       <l-tile-layer :url="url" :attribution="attribution" />
       <l-marker
         v-for="settlement in settlements"
-        :lat-lng="[settlement.position.coordinates[0], settlement.position.coordinates[1]]"
+        :lat-lng="[settlement.position.coordinates[1], settlement.position.coordinates[0]]"
         :key="settlement.id"
+        :icon-url="IconeImg"
+        :icon-size="[16, 16]"
       ></l-marker>
     </l-map>
   </div>
@@ -24,6 +28,16 @@ import { latLng, latLngBounds, CRS } from 'leaflet';
 import { LMap, LTileLayer, LMarker, LPopup, LTooltip, LIcon } from 'vue2-leaflet';
 import strategusModule from '@/store/strategus-module';
 import Settlement from '@/models/settlement';
+import IconeImg from '@/assets/map-marker-icon.png';
+import { Icon } from 'leaflet';
+
+delete (Icon.Default.prototype as any)._getIconUrl;
+
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 @Component({
   components: { LMap, LTileLayer, LMarker, LPopup, LTooltip, LIcon },
@@ -39,18 +53,36 @@ export default class Strategus extends Vue {
     maxZoom: 8,
     crs: CRS.Simple,
   };
+  IconeImg = IconeImg;
   maxBounds = latLngBounds([
     [-40.6, 5.1],
     [-215.4, 250.8],
   ]);
-  infoPOS = null;
+  displayedBound = null;
 
   get settlements(): Settlement[] {
-    return strategusModule.settlements;
+    if (this.displayedBound) {
+      return strategusModule.settlements.filter(
+        settlement =>
+          this.displayedBound.contains(
+            latLng(settlement.position.coordinates[1], settlement.position.coordinates[0])
+          ) &&
+          ((this.zoom > 5 && ['Village', 'Town', 'Castle'].includes(settlement.type)) ||
+            (this.zoom <= 5 && ['Castle'].includes(settlement.type)))
+      );
+    }
+    return [];
+  }
+
+  setVisibleBounds(bounds: any) {
+    this.displayedBound = bounds;
   }
 
   created() {
     strategusModule.getSettlements();
+  }
+  mounted() {
+    this.setVisibleBounds(this.$refs.map.mapObject.getBounds());
   }
 
   infoPos(event: any) {
