5cdf5f630edb576ec19a0a50f3ab078c535db9ab
verdie-g <gregoire.verdier@gmail.com>
2021-03-28 21:06:25 +0200

    webui: increase hero marker size depending on its troops

--- src/WebUI/src/components/strategus/HeroComponent.vue
+++ src/WebUI/src/components/strategus/HeroComponent.vue
@@ -2,7 +2,7 @@
   <l-circle-marker
     v-if="hero"
     :lat-lng="[hero.position.coordinates[1], hero.position.coordinates[0]]"
-    :radius="5"
+    :radius="markerRadius"
     color="#f00"
     :fill="true"
     fillColor="#f00"
@@ -14,12 +14,26 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { LCircleMarker } from 'vue2-leaflet';
 import Hero from '@/models/hero';
+import Constants from '@/../../../data/constants.json';
+
+const minRadius = 4;
+const maxRadius = 10;
 
 @Component({
   components: { LCircleMarker },
 })
 export default class HeroComponent extends Vue {
-  @Prop(Object) readonly hero: Hero;
+  @Prop(Object) readonly hero: Hero | null;
+
+  get markerRadius(): number {
+    if (this.hero === null) {
+      return 0;
+    }
+
+    const troopsRange = Constants.strategusMaxHeroTroops - Constants.strategusMinHeroTroops;
+    const sizeFactor = this.hero.troops / troopsRange;
+    return minRadius + sizeFactor * (maxRadius - minRadius);
+  }
 }
 </script>
 
