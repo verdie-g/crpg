bf2b4318e55ae249ec05dcd6873501fcf26469ce
Namidaka <worldofmaps@gmail.com>
2021-04-06 07:24:14 +0200

    Initializing List with element already in it

--- test/Application.UTest/Common/Services/StrategusSpeedModelTest.cs
+++ test/Application.UTest/Common/Services/StrategusSpeedModelTest.cs
@@ -17,15 +17,21 @@ namespace Crpg.Application.UTest.Common.Services
             var mount1 = new StrategusOwnedItem() { Item = new Item() { Mount = new ItemMountComponent() { HitPoints = 250 } }, Count = 1 };
             var mount2 = new StrategusOwnedItem() { Item = new Item() { Mount = new ItemMountComponent() { HitPoints = 250 } }, Count = 2 };
             var mount3 = new StrategusOwnedItem() { Item = new Item() { Mount = new ItemMountComponent() { HitPoints = 250 } }, Count = 3 };
-            hero1.OwnedItems = new List<StrategusOwnedItem>();
-            hero1.OwnedItems.Add(mount1);
-            hero2.OwnedItems = new List<StrategusOwnedItem>();
-            hero2.OwnedItems.Add(mount2);
-            hero3.OwnedItems = new List<StrategusOwnedItem>();
-            hero3.OwnedItems.Add(mount3);
+            hero1.OwnedItems = new List<StrategusOwnedItem>
+            {
+                mount1
+            };
+            hero2.OwnedItems = new List<StrategusOwnedItem>
+            {
+                mount2
+            };
+            hero3.OwnedItems = new List<StrategusOwnedItem>
+            {
+                mount3
+            };
             var speedModel = new StrategusSpeedModel();
-            Assert.Greater(speedModel.ComputeHeroSpeed(hero2), speedModel.ComputeHeroSpeed(hero1));
-            Assert.Greater(speedModel.ComputeHeroSpeed(hero3), speedModel.ComputeHeroSpeed(hero2));
+            Assert.That(speedModel.ComputeHeroSpeed(hero2), Is.GreaterThan(speedModel.ComputeHeroSpeed(hero1)));
+            Assert.That(speedModel.ComputeHeroSpeed(hero3), Is.GreaterThan(speedModel.ComputeHeroSpeed(hero2)));
         }
     }
 }
