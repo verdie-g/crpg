2153d628135be6739d46c3144031b8e5897efb61
verdie-g <gregoire.verdier@gmail.com>
2021-06-21 10:39:10 +0200

    webapi: document BattleMercenary

--- src/Domain/Entities/Battles/BattleMercenary.cs
+++ src/Domain/Entities/Battles/BattleMercenary.cs
@@ -8,17 +8,30 @@ namespace Crpg.Domain.Entities.Battles
     public class BattleMercenary
     {
         public int Id { get; set; }
+
+        /// <summary>The id of the battle the user will fight in.</summary>
         public int BattleId { get; set; }
 
         /// <summary>The id of the character the user will fight with.</summary>
         public int CharacterId { get; set; }
         public BattleSide Side { get; set; }
+
+        /// <summary>The id of the fighter the user will fight for.</summary>
         public int CaptainFighterId { get; set; }
+
+        /// <summary>The id of the application that got the user accepted in the battle.</summary>
         public int ApplicationId { get; set; }
 
+        /// <summary>See <see cref="CharacterId"/>.</summary>
         public Character? Character { get; set; }
+
+        /// <summary>See <see cref="BattleId"/>.</summary>
         public Battle? Battle { get; set; }
+
+        /// <summary>See <see cref="CaptainFighterId"/>.</summary>
         public BattleFighter? CaptainFighter { get; set; }
+
+        /// <summary>See <see cref="ApplicationId"/>.</summary>
         public BattleMercenaryApplication? Application { get; set; }
     }
 }
