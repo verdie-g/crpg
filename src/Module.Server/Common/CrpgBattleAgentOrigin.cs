64d3b451de5397d17cb87237c3735b4e331a1b95
verdie-g <gregoire.verdier@gmail.com>
2022-09-01 16:07:13 +0200

    mod: hack to support crpg characteristics

---
+++ src/Module.Server/Common/CrpgBattleAgentOrigin.cs
@@ -0,0 +1,15 @@
+﻿using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace Crpg.Module.Common;
+
+internal class CrpgBattleAgentOrigin : BasicBattleAgentOrigin
+{
+    public CharacterSkills Skills { get; }
+
+    public CrpgBattleAgentOrigin(BasicCharacterObject? troop, CharacterSkills skills)
+        : base(troop)
+    {
+        Skills = skills;
+    }
+}
