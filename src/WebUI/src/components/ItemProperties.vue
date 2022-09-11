370187c34ed372c9f703c1fa3b29ddd61ad43a92
Luke Roberts <luke@mewt.eu>
2022-09-11 02:09:58 +0100

    Make layout of shop cards a bit more clear and consistent

--- src/WebUI/src/components/ItemProperties.vue
+++ src/WebUI/src/components/ItemProperties.vue
@@ -1,17 +1,35 @@
 <template>
-  <div>
-    <div v-for="field in itemDescriptor.fields" :key="field[0]">{{ field[0] }}: {{ field[1] }}</div>
-    <b-tabs v-if="itemDescriptor.modes.length" :value="weaponIdx" class="weapon-tabs">
+  <div class="is-flex is-flex-direction-column is-flex-grow-1">
+    <div class="columns is-flex-wrap-wrap">
+      <div v-for="field in itemDescriptor.fields" :key="field[0]" class="column is-half is-flex is-flex-direction-column">
+        <span class="is-size-7">{{ field[0] }}</span>
+        <strong>{{ field[1] }}</strong>
+      </div>
+    </div>
+    <b-tabs v-if="itemDescriptor.modes.length > 1" :value="weaponIdx" class="weapon-tabs">
       <b-tab-item v-for="mode in itemDescriptor.modes" :key="mode.name" :label="mode.name">
         <div v-for="field in mode.fields">
-          {{ field[0] }}: {{ field[1] }}
+          <strong>{{ field[1] }}</strong> {{ field[0] }}
           <br />
         </div>
-        <b-taglist class="flags pt-2">
+        <b-taglist class="flags px-1 pt-2">
           <b-tag v-for="flag in mode.flags" :key="flag" type="is-info">{{ flag }}</b-tag>
         </b-taglist>
       </b-tab-item>
     </b-tabs>
+    <div v-else class="is-flex is-flex-direction-column is-flex-grow-1">
+      <div v-for="mode in itemDescriptor.modes" :key="mode.name" class="is-flex is-flex-direction-column is-flex-grow-1">
+        <div class="is-flex-grow-1">
+          <div v-for="field in mode.fields">
+            <strong>{{ field[1] }}</strong> {{ field[0] }}
+            <br />
+          </div>
+        </div>
+        <b-taglist class="flags px-1 pt-2">
+          <b-tag v-for="flag in mode.flags" :key="flag" type="is-info">{{ flag }}</b-tag>
+        </b-taglist>
+      </div>
+    </div>
   </div>
 </template>
 
