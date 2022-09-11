d090d2d6a3dc8efa85563ea99d8d5a7a7e0016a8
verdie-g <gregoire.verdier@gmail.com>
2022-09-12 01:52:24 +0200

    webapi: add avatar to user public model

--- src/Application/Users/Models/UserPublicViewModel.cs
+++ src/Application/Users/Models/UserPublicViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Crpg.Application.Common.Mappings;
+﻿using AutoMapper;
+using Crpg.Application.Common.Mappings;
 using Crpg.Domain.Entities.Users;
 
 namespace Crpg.Application.Users.Models;
@@ -9,4 +10,11 @@ public record UserPublicViewModel : IMapFrom<User>
     public Platform Platform { get; init; }
     public string PlatformUserId { get; init; } = string.Empty;
     public string Name { get; init; } = string.Empty;
+    public Uri? Avatar { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<User, UserPublicViewModel>()
+            .ForMember(u => u.Avatar, opt => opt.MapFrom(u => u.AvatarMedium));
+    }
 }
