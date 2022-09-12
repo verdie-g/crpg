795f299477e80f8a26ef9166e7d7a564dd69207a
Milan Döpper <milan.doepper@gmail.com>
2022-09-13 00:52:40 +0200

    Added enemy focus name

--- src/Module.Server/Battle/CrpgBattleMissionMultiplayerClient.cs
+++ src/Module.Server/Battle/CrpgBattleMissionMultiplayerClient.cs
@@ -44,6 +44,10 @@ internal class CrpgBattleMissionMultiplayerClient : MissionMultiplayerGameModeBa
         base.OnMissionTick(dt);
     }
 
+    public override void AfterStart()
+    {
+        Mission.Current.SetMissionMode(TaleWorlds.Core.MissionMode.Battle, true);
+    }
 
     protected override void AddRemoveMessageHandlers(
         GameNetwork.NetworkMessageHandlerRegistererContainer registerer)
