4cc683549db6eb5bee732d751add59b0e1930f50
Milan Döpper <milan.doepper@gmail.com>
2022-09-13 00:23:12 +0200

    Implemented shield dmg reduction depending on shield skill

---
+++ src/Module.Server/Common/Models/CrpgAgentApplyDamageModel.cs
@@ -0,0 +1,35 @@
+﻿using Crpg.Module.Helpers;
+using TaleWorlds.MountAndBlade;
+
+namespace Crpg.Module.Common.Models;
+
+/// <summary>
+/// Used to adjust dmg calculations.
+/// </summary>
+internal class CrpgAgentApplyDamageModel : DefaultAgentApplyDamageModel
+{
+    private readonly CrpgConstants _constants;
+
+    public CrpgAgentApplyDamageModel(CrpgConstants constants)
+    {
+        _constants = constants;
+    }
+
+    // public override float CalculateShieldDamage only has dmg as parameter. Therefore it cannot be used to get any Skill values.
+    public override float CalculateDamage(in AttackInformation attackInformation, in AttackCollisionData collisionData, in MissionWeapon weapon, float baseDamage)
+    {
+        float finalDamage = baseDamage;
+        if (collisionData.AttackBlockedWithShield)
+        {
+            int shieldSkill = 0;
+            if (attackInformation.VictimAgentOrigin is CrpgBattleAgentOrigin crpgOrigin)
+            {
+                shieldSkill = crpgOrigin.Skills.GetPropertyValue(CrpgSkills.Shield);
+            }
+
+            finalDamage /= MathHelper.ApplyPolynomialFunction(shieldSkill, _constants.DurabilityFactorForShieldCoefs);
+        }
+
+        return finalDamage;
+    }
+}
