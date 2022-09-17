efc991231cfc003ef6278d468e8be79ae490e241
verdie-g <gregoire.verdier@gmail.com>
2022-09-17 12:28:48 +0200

    webapi: enable respec penalty (fix #297)

--- src/WebUI/src/components/CharacterComponent.vue
+++ src/WebUI/src/components/CharacterComponent.vue
@@ -120,7 +120,7 @@
 
       <br />
 
-      <b-tooltip label="Respecialize character." multilined>
+      <b-tooltip label="Respecialize character for a third of its experience." multilined>
         <b-button
           type="is-warning"
           icon-left="angle-double-down"
