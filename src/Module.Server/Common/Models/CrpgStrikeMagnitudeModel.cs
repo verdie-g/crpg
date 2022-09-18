73e687516c204d71b928a1589598bf921312bcc7
GerGotha <Milan.Doepper@gmail.com>
2022-09-18 20:48:52 +0200

    mod: temporary buff damage types (#316 fixes #303)

---
+++ src/Module.Server/Common/Models/CrpgStrikeMagnitudeModel.cs
@@ -0,0 +1,38 @@
+﻿using Crpg.Module.Helpers;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+namespace Crpg.Module.Common.Models;
+
+/// <summary>
+/// Used to adjust raw dmg calculations.
+/// </summary>
+internal class CrpgStrikeMagnitudeModel : MultiplayerStrikeMagnitudeModel
+{
+    private readonly CrpgConstants _constants;
+
+    public CrpgStrikeMagnitudeModel(CrpgConstants constants)
+    {
+        _constants = constants;
+    }
+
+    public override float GetBluntDamageFactorByDamageType(DamageTypes damageType)
+    {
+        float result = 0f;
+        switch (damageType)
+        {
+            case DamageTypes.Blunt:
+                result = 1f; // Native 1f
+                break;
+            case DamageTypes.Cut:
+                result = 0.35f; // Native .1f
+                break;
+            case DamageTypes.Pierce:
+                result = .4f; // Native .25f
+                break;
+        }
+
+        return result;
+    }
+}
