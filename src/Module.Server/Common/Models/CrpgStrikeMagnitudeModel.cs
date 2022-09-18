dc4ee772f5758786e340841548ed4d4f32537f02
Milan Döpper <milan.doepper@gmail.com>
2022-09-18 20:04:00 +0200

    Removed duplicated override

--- src/Module.Server/Common/Models/CrpgStrikeMagnitudeModel.cs
+++ src/Module.Server/Common/Models/CrpgStrikeMagnitudeModel.cs
@@ -17,34 +17,6 @@ internal class CrpgStrikeMagnitudeModel : MultiplayerStrikeMagnitudeModel
         _constants = constants;
     }
 
-    public override float ComputeRawDamage(DamageTypes damageType, float magnitude, float armorEffectiveness, float absorbedDamageRatio)
-    {
-        float bluntDamageFactorByDamageType = GetBluntDamageFactorByDamageType(damageType);
-        float num = 100f / (100f + armorEffectiveness);
-        float num2 = magnitude * num;
-        float num3 = bluntDamageFactorByDamageType * num2;
-        if (damageType != DamageTypes.Blunt)
-        {
-            float num4;
-            switch (damageType)
-            {
-                case DamageTypes.Cut:
-                    num4 = MathF.Max(0f, magnitude * (1f - 0.6f * armorEffectiveness / (20f + 0.4f * armorEffectiveness)));
-                    break;
-                case DamageTypes.Pierce:
-                    num4 = MathF.Max(0f, magnitude * (45f / (45f + armorEffectiveness)));
-                    break;
-                default:
-                    Debug.FailedAssert("Given damage type is invalid.", "C:\\Develop2\\mb3\\Source\\Bannerlord\\TaleWorlds.MountAndBlade\\ComponentInterfaces\\MultiplayerStrikeMagnitudeModel.cs", "ComputeRawDamage", 45);
-                    return 0f;
-            }
-
-            num3 += (1f - bluntDamageFactorByDamageType) * num4;
-        }
-
-        return num3 * absorbedDamageRatio;
-    }
-
     public override float GetBluntDamageFactorByDamageType(DamageTypes damageType)
     {
         float result = 0f;
