5e16e82ffc04d7debd80e7e16ede6efac8169000
Milan Döpper <milan.doepper@gmail.com>
2022-09-21 00:59:39 +0200

    Added ban command (ban & mute are still missing web call) Added admin / mod check to admin functions

--- src/Module.Server/Common/ChatCommands/AdminCommands/AdminCmd.cs
+++ src/Module.Server/Common/ChatCommands/AdminCommands/AdminCmd.cs
@@ -1,5 +1,4 @@
-﻿using Crpg.Module.Common.GameHandler;
-using TaleWorlds.MountAndBlade;
+﻿using TaleWorlds.MountAndBlade;
 
 namespace Crpg.Module.Common.ChatCommands.UserCommands;
 internal abstract class AdminCmd : ChatCommand
@@ -7,16 +6,21 @@ internal abstract class AdminCmd : ChatCommand
     public AdminCmd()
         : base()
     {
-        Command = string.Empty;
-        PatternList = new Pattern[] { }.ToList();
     }
 
     protected override bool CheckRequirements(NetworkCommunicator fromPeer)
     {
-        bool hasAccess = base.CheckRequirements(fromPeer);
         // TODO: Add cRPG admin check
-        hasAccess = true;
-        return hasAccess;
+        var crpgRepresentative = fromPeer.GetComponent<CrpgRepresentative>();
+        var user = crpgRepresentative?.User;
+        if (user == null
+            || ((user?.Role.HasFlag(Api.Models.Users.CrpgUserRole.Admin) ?? false)
+            && (user?.Role.HasFlag(Api.Models.Users.CrpgUserRole.Moderator) ?? false)))
+        {
+            return false;
+        }
+
+        return true;
     }
 
 }
--- src/Module.Server/Common/ChatCommands/AdminCommands/MuteCmd.cs
+++ src/Module.Server/Common/ChatCommands/AdminCommands/MuteCmd.cs
@@ -1,16 +1,27 @@
-﻿using Crpg.Module.Common.GameHandler;
+﻿using Crpg.Module.Api.Models;
+using Crpg.Module.Common.GameHandler;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.MountAndBlade.Diamond;
 
 namespace Crpg.Module.Common.ChatCommands.UserCommands;
 internal class MuteCmd : AdminCmd
 {
+    private enum MuteDuration : int
+    {
+        Minutes = 1,
+        Hours = 60,
+        Days = 1440,
+    }
+
     public MuteCmd()
         : base()
     {
         Command = "mute";
-        Description = $"'{ChatCommandHandler.CommandPrefix}{Command} PLAYERID' to kick a player.";
-        PatternList = new Pattern[] { new Pattern("p", ExecuteMuteByNetworkPeer), new Pattern("s", ExecuteMuteByName) }.ToList();
+        Description = $"'{ChatCommandHandler.CommandPrefix}{Command} PLAYERID' to mute a player.";
+        PatternList = new Pattern[] {
+            new Pattern("dps", ExecuteMuteByNetworkPeer), // !mute PLAYERID DURATION REASON
+            new Pattern("sds", ExecuteMuteByName), // !mute NamePattern DURATION REASON
+        }.ToList();
     }
 
     protected override void ExecuteFailed(NetworkCommunicator fromPeer)
@@ -23,9 +34,40 @@ internal class MuteCmd : AdminCmd
     {
         CrpgChatBox crpgChat = GetChat();
         var targetPeer = (NetworkCommunicator)parameters[0];
-        crpgChat.ServerSendMessageToPlayer(fromPeer, ChatCommandHandler.ColorFatal, $"You {(targetPeer.IsMuted ? "unmuted" : "muted")}: {targetPeer.UserName}");
-        targetPeer.IsMuted = !targetPeer.IsMuted;
+        double duration = (double)parameters[1];
+        string reason = (string)parameters[2];
+        MuteDuration durationType = MuteDuration.Days;
+        if (parameters.Count == 4)
+        {
+            durationType = (MuteDuration)parameters[3];
+        }
+
+        DateTime muteUntilDate = DateTime.Now;
+        muteUntilDate.AddMinutes(duration * (int)durationType);
+
+
         // Call mute for backend
+        var adminCrpgRepresentative = fromPeer.GetComponent<CrpgRepresentative>();
+        var victimCrpgRepresentative = targetPeer.GetComponent<CrpgRepresentative>();
+        if (adminCrpgRepresentative?.User?.Character == null && victimCrpgRepresentative?.User?.Character == null)
+        {
+            return;
+        }
+
+        // TODO: Add web request to save the restriction
+        // Call webrequest. Muted until muteUntilDate
+        if (duration == 0)
+        {
+            crpgChat.ServerSendMessageToPlayer(fromPeer, ChatCommandHandler.ColorSuccess, $"You were unmuted by {fromPeer.UserName}.");
+            crpgChat.ServerSendMessageToPlayer(fromPeer, ChatCommandHandler.ColorSuccess, $"You muted {targetPeer.UserName} until {muteUntilDate.ToString("dd.MM.yy HH:mm")}.");
+            targetPeer.IsMuted = false;
+        }
+        else
+        {
+            crpgChat.ServerSendMessageToPlayer(targetPeer, ChatCommandHandler.ColorFatal, $"You were muted by {fromPeer.UserName} until {muteUntilDate.ToString("dd.MM.yy HH:mm")}.");
+            crpgChat.ServerSendMessageToPlayer(fromPeer, ChatCommandHandler.ColorFatal, $"You muted {targetPeer.UserName} until {muteUntilDate.ToString("dd.MM.yy HH:mm")}.");
+            targetPeer.IsMuted = true;
+        }
     }
 
     private void ExecuteMuteByName(NetworkCommunicator fromPeer, string cmd, List<object> parameters)
