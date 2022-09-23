15e0b8325c865c09aa39895ad3a5d32346374792
Namidaka <worldofmaps@gmail.com>
2022-09-23 23:07:37 +0200

    more merge conflicts solving

--- src/WebUI/src/components/CharacterStatsComponent.vue
+++ src/WebUI/src/components/CharacterStatsComponent.vue
@@ -51,7 +51,7 @@
           size="is-small"
           :editable="false"
           controls-position="compact"
-          :value="computeHealthPoints()"
+          :value="HealthPoints"
           :controls="false"
         />
       </b-field>
@@ -602,7 +602,7 @@ export default class CharacterCharacteristicsComponent extends Vue {
         : Math.round((100 * (statistics.kills + statistics.assists)) / statistics.deaths) / 100;
     return `${statistics.kills}/${statistics.deaths}/${statistics.assists} (${ratio})`;
   }
-  computeHealthPoints(): number {
+  get HealthPoints(): number {
     return computeHealthPoints(
       this.getInputProps('skills', 'ironFlesh').value,
       this.getInputProps('attributes', 'strength').value
