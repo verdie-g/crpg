a502dd62816c3080decf1b178695cde94c9d4506
Namidaka <worldofmaps@gmail.com>
2022-09-27 15:54:44 +0200

    item.ItemComponent.GetName().ToString() -->item.Name.ToString()

--- src/Module.Server/Common/Models/crpgItemRequirementModel.cs
+++ src/Module.Server/Common/Models/crpgItemRequirementModel.cs
@@ -14,8 +14,8 @@ internal static class CrpgItemRequirementModel
     {
         int strengthRequirementForTierTenArmor = 24; // Tiers are calulated in CrpgValueModel. 0<Tier=<10 . By design the best armor is always at Ten.
         if (item.ArmorComponent == null)
-        {
-            throw new ArgumentException(item.ItemComponent.GetName().ToString(), "is not an armor item");
+        {z
+            throw new ArgumentException(item.Name.ToString(), "is not an armor item");
         }
 
         double doubleRequirement = item.Tierf * (float)(strengthRequirementForTierTenArmor / 10f);
