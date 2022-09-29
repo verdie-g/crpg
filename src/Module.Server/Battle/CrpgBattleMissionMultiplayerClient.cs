de9d3b45ae41440e03c885a75829a2124dad1354
verdie-g <gregoire.verdier@gmail.com>
2022-09-30 00:06:42 +0200

    mod: compact gain and upkeep message

--- src/Module.Server/Battle/CrpgBattleMissionMultiplayerClient.cs
+++ src/Module.Server/Battle/CrpgBattleMissionMultiplayerClient.cs
@@ -291,16 +291,15 @@ internal class CrpgBattleMissionMultiplayerClient : MissionMultiplayerGameModeBa
                 new Color(218, 112, 214)));
         }
 
-        if (reward.Gold != 0)
+        int gain = reward.Gold - message.RepairCost;
+        if (gain != 0)
         {
-            InformationManager.DisplayMessage(new InformationMessage($"Gained {reward.Gold} gold.",
-                new Color(65, 105, 225)));
-        }
-
-        if (message.RepairCost != 0)
-        {
-            InformationManager.DisplayMessage(new InformationMessage($"Lost {message.RepairCost} gold for upkeep.",
-                new Color(0.74f, 0.28f, 0.01f)));
+            (Color color, string verb) = gain > 0
+                ? (new Color(65, 105, 225), "Gained")
+                : (new Color(0.74f, 0.28f, 0.01f), "Lost");
+            InformationManager.DisplayMessage(
+                new InformationMessage($"{verb} {reward.Gold} gold (reward: {gain}, upkeep: {message.RepairCost}).",
+                    color));
         }
 
         if (message.SoldItemIds.Count != 0)
