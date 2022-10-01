dd1552fb0b6e4343514237eac424d6d1d3694166
Milan Döpper <milan.doepper@gmail.com>
2022-10-01 02:30:39 +0200

    AnnouncementCommand commit

---
+++ src/Module.Server/Common/ChatCommands/Admin/AnnouncementCommand.cs
@@ -0,0 +1,40 @@
+﻿using Crpg.Module.Common.GameHandler;
+using Crpg.Module.Common.Network;
+using TaleWorlds.MountAndBlade;
+
+namespace Crpg.Module.Common.ChatCommands.Admin;
+
+internal class AnnouncementCommand : AdminCommand
+{
+    public AnnouncementCommand()
+    {
+        Name = "a";
+        Description = $"'{ChatCommandHandler.CommandPrefix}{Name} message' to send an admin announcement.";
+        Overloads = new CommandOverload[]
+        {
+            new(new[] { ChatCommandParameterType.String }, ExecuteAnnouncement),
+        };
+    }
+
+    protected override void ExecuteFailed(NetworkCommunicator fromPeer)
+    {
+        CrpgChatBox crpgChat = GetChat();
+        crpgChat.ServerSendMessageToPlayer(fromPeer, ColorInfo, $"Wrong usage. Type {Description}");
+    }
+
+    private void ExecuteAnnouncement(NetworkCommunicator fromPeer, string cmd, object[] arguments)
+    {
+        string message = $"{fromPeer.UserName}: {(string)arguments[0]}";
+
+        foreach (NetworkCommunicator targetPeer in GameNetwork.NetworkPeers.Where((NetworkCommunicator x) => !x.IsServerPeer && x.IsSynchronized))
+        {
+            GameNetwork.BeginModuleEventAsServer(targetPeer);
+            GameNetwork.WriteMessage(new CrpgNotification
+            {
+                Type = CrpgNotification.NotificationType.Announcement,
+                Message = message,
+            });
+            GameNetwork.EndModuleEventAsServer();
+        }
+    }
+}
