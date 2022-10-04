c95f6cfcbe7170ab59b89a96d6b8a82c7b2b5b49
Valery Sinevich <zohan33333@gmail.com>
2022-10-04 02:23:13 +0000

    some fixes

--- src/WebUINext/src/boot/i18n.ts
+++ src/WebUINext/src/boot/i18n.ts
@@ -5,20 +5,20 @@ import { type BootModule } from '~/types/boot';
 // https://vitejs.dev/guide/features.html#glob-import
 
 const messages = Object.fromEntries(
-  Object.entries(import.meta.glob<{ default: any }>('~~/locales/*.y(a)?ml', { eager: true })).map(
-    ([key, value]) => {
-      const yaml = key.endsWith('.yaml');
-      return [key.slice(14, yaml ? -5 : -4), value.default];
-    }
-  )
+  Object.entries(
+    import.meta.glob<{ default: any }>('../../locales/*.y(a)?ml', { eager: true })
+  ).map(([key, value]) => {
+    const yaml = key.endsWith('.yaml');
+    return [key.slice(14, yaml ? -5 : -4), value.default];
+  })
 );
 
-export const install: BootModule = app => {
-  const i18n = createI18n({
-    legacy: false,
-    locale: 'en',
-    messages,
-  });
+export const i18n = createI18n({
+  legacy: false,
+  locale: 'en',
+  messages,
+});
 
+export const install: BootModule = app => {
   app.use(i18n);
 };
