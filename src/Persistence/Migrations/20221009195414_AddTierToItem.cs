c31f79595b5d7cf91558767416514f6815cbcd1e
Namidaka <worldofmaps@gmail.com>
2022-10-11 14:57:03 +0200

    format document

--- src/Persistence/Migrations/20221009195414_AddTierToItem.cs
+++ src/Persistence/Migrations/20221009195414_AddTierToItem.cs
@@ -7,19 +7,19 @@ namespace Crpg.Persistence.Migrations;
 public partial class AddTierToItem : Migration
 {
     protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<float>(
-                name: "tier",
-                table: "items",
-                type: "real",
-                nullable: false,
-                defaultValue: 0f);
-        }
+    {
+        migrationBuilder.AddColumn<float>(
+            name: "tier",
+            table: "items",
+            type: "real",
+            nullable: false,
+            defaultValue: 0f);
+    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "tier",
-                table: "items");
-        }
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "tier",
+            table: "items");
+    }
 }
