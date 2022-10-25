16180fc722cf4ca8a44ef3df448d2b6ceddb86e2
verdie-g <gregoire.verdier@gmail.com>
2022-10-25 17:53:55 +0200

    mod: throw proper exception on bad flag indexes

--- src/Module.Server/Battle/CrpgBattleMissionMultiplayer.cs
+++ src/Module.Server/Battle/CrpgBattleMissionMultiplayer.cs
@@ -193,11 +193,27 @@ internal class CrpgBattleMissionMultiplayer : MissionMultiplayerGameModeBase
     private void ResetFlags()
     {
         _flags = Mission.Current.MissionObjects.FindAllWithType<FlagCapturePoint>().ToArray();
+        ThrowOnBadFlagIndexes(_flags);
         _flagOwners = new Team[_flags.Length];
         _agentCountsAroundFlags = new int[_flags.Length, (int)BattleSideEnum.NumSides];
         foreach (var flag in _flags)
         {
             flag.ResetPointAsServer(FlagNeutralColor1, FlagNeutralColor2);
+        }
+    }
+
+    /// <summary>Checks the flag index are from 0 to N.</summary>
+    private void ThrowOnBadFlagIndexes(FlagCapturePoint[] flags)
+    {
+        int expectedIndex = 0;
+        foreach (var flag in flags.OrderBy(f => f.FlagIndex))
+        {
+            if (flag.FlagIndex != expectedIndex)
+            {
+                throw new Exception($"Invalid scene '{Mission.SceneName}': Flag indexes should be numbered from 0 to {flags.Length}");
+            }
+
+            expectedIndex += 1;
         }
     }
 
