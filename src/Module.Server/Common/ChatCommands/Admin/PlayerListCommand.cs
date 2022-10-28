e40b8c9cace0de93a0241dd3d5dfaf8a5c22dbfb
verdie-g <gregoire.verdier@gmail.com>
2022-10-28 18:19:41 +0200

    mod: add filter on player list command (fix #521)

--- src/Module.Server/Common/ChatCommands/Admin/PlayerListCommand.cs
+++ src/Module.Server/Common/ChatCommands/Admin/PlayerListCommand.cs
@@ -8,17 +8,25 @@ internal class PlayerListCommand : AdminCommand
         : base(chatComponent)
     {
         Name = "pl";
+        Description = $"'{ChatCommandsComponent.CommandPrefix}{Name} [NAMEFILTER] to list players.";
         Overloads = new CommandOverload[]
         {
-            new(Array.Empty<ChatCommandParameterType>(), ExecuteSuccess),
+            new(new[] { ChatCommandParameterType.String }, Execute),
+            new(Array.Empty<ChatCommandParameterType>(), Execute),
         };
     }
 
-    private void ExecuteSuccess(NetworkCommunicator fromPeer, object[] arguments)
+    private void Execute(NetworkCommunicator fromPeer, object[] arguments)
     {
+        string? filter = arguments.Length > 0 ? ((string)arguments[0]).ToLower() : null;
         ChatComponent.ServerSendMessageToPlayer(fromPeer, ColorInfo, "- Players -");
         foreach (NetworkCommunicator networkPeer in GameNetwork.NetworkPeers)
         {
+            if (filter != null && !networkPeer.UserName.ToLower().Contains(filter))
+            {
+                continue;
+            }
+
             var crpgRepresentative = networkPeer.GetComponent<CrpgRepresentative>();
             if (networkPeer.IsSynchronized && crpgRepresentative.User != null)
             {
