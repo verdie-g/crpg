4972457dcc3e784254d799acdedfbf59058c3c50
Namidaka <worldofmaps@gmail.com>
2022-10-31 17:15:46 +0100

    Rework too make strength the go to when wearing heavy armor while still giving balanced build a diversite of choice

--- src/WebUI/src/services/characters-service.ts
+++ src/WebUI/src/services/characters-service.ts
@@ -30,11 +30,15 @@ export function computeSpeedStats({
   agility: number;
   totalEncumbrance: number;
 }): CharacterSpeedStats {
-  const weightReductionFactor = 1 / (1 + (strength - 3) / 12);
+  const weightReductionFactor = 1 / (1 + (strength * strength - 9) / 81);
   const freeWeight = 3 * (1 + (strength - 3) / 30);
   const perceivedWeight = Math.max(totalEncumbrance - freeWeight, 0) * weightReductionFactor;
   const nakedSpeed = 0.7 + 0.00085 * (20 * athletics + 3 * agility);
-  const сurrentSpeed = clamp(nakedSpeed * (1 - perceivedWeight / 70), 0.1, 1.5);
+  const сurrentSpeed = clamp(
+    nakedSpeed * Math.pow(361 / (361 + Math.pow(perceivedWeight, 5)), 0.05),
+    0.1,
+    1.5
+  );
   const timeToMaxSpeed =
     1.4 *
     (1 + perceivedWeight / 40) *
