6533beae4be1910a36aaf36bf2f75b63f6b2b0a2
Namidaka <worldofmaps@gmail.com>
2022-11-01 16:19:15 +0100

    .

--- src/Module.Server/Common/Models/CrpgAgentStatCalculateModel.cs
+++ src/Module.Server/Common/Models/CrpgAgentStatCalculateModel.cs
@@ -288,7 +288,7 @@ internal class CrpgAgentStatCalculateModel : AgentStatCalculateModel
         float totalEncumbrance = props.ArmorEncumbrance + props.WeaponsEncumbrance;
         float freeWeight = 3f * (1 + (strengthSkill - 3f) / 30f);
         float perceivedWeight = Math.Max(totalEncumbrance - freeWeight, 0f) * weightReductionFactor;
-        props.TopSpeedReachDuration = 1.5f * (1f + perceivedWeight / 40f) * (20f / (20f + (float)Math.Pow(athleticsSkill / 100f, 2f)));
+        props.TopSpeedReachDuration = 1.4f * (1f + perceivedWeight / 30f) * (20f / (20f + (float)Math.Pow(athleticsSkill / 100f, 2f)));
         float speed = 0.7f + 0.00085f * athleticsSkill;
         props.MaxSpeedMultiplier = MBMath.ClampFloat(
             speed * (float)Math.Pow(361f / (361f + (float)Math.Pow(perceivedWeight, 5f)), 0.05f),
--- src/WebUI/src/services/characters-service.ts
+++ src/WebUI/src/services/characters-service.ts
@@ -40,8 +40,8 @@ export function computeSpeedStats({
     1.5
   );
   const timeToMaxSpeed =
-    1.5 *
-    (1 + perceivedWeight / 40) *
+    1.4 *
+    (1 + perceivedWeight / 30) *
     (20 / (20 + Math.pow((20 * athletics + 3 * agility) / 100, 2)));
 
   return {
