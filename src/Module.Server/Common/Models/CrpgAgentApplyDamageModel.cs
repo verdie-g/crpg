508800366d3d73a642417f122414ac16d7336ae8
verdie-g <gregoire.verdier@gmail.com>
2022-11-01 14:27:03 +0100

    mod: reformat CrpgAgentApplyDamageModel

--- src/Module.Server/Common/Models/CrpgAgentApplyDamageModel.cs
+++ src/Module.Server/Common/Models/CrpgAgentApplyDamageModel.cs
@@ -18,7 +18,11 @@ internal class CrpgAgentApplyDamageModel : DefaultAgentApplyDamageModel
     }
 
     // public override float CalculateShieldDamage only has dmg as parameter. Therefore it cannot be used to get any Skill values.
-    public override float CalculateDamage(in AttackInformation attackInformation, in AttackCollisionData collisionData, in MissionWeapon weapon, float baseDamage)
+    public override float CalculateDamage(
+        in AttackInformation attackInformation,
+        in AttackCollisionData collisionData,
+        in MissionWeapon weapon,
+        float baseDamage)
     {
         float finalDamage = base.CalculateDamage(attackInformation, collisionData, weapon, baseDamage);
         if (collisionData.AttackBlockedWithShield && finalDamage > 0)
@@ -35,7 +39,14 @@ internal class CrpgAgentApplyDamageModel : DefaultAgentApplyDamageModel
         if (!weapon.IsEmpty)
         {
             // Bonus dmg with spears against horses (does only work with "main" spears - not javelins etc)
-            if (!attackInformation.IsVictimAgentHuman && !attackInformation.DoesAttackerHaveMountAgent && weapon.CurrentUsageItem.IsPolearm && !weapon.CurrentUsageItem.IsConsumable && weapon.CurrentUsageItem.IsMeleeWeapon && collisionData.StrikeType == (int)StrikeType.Thrust && collisionData.DamageType == (int)DamageTypes.Pierce && !weapon.IsAnyConsumable())
+            if (!attackInformation.IsVictimAgentHuman
+                && !attackInformation.DoesAttackerHaveMountAgent
+                && weapon.CurrentUsageItem.IsPolearm
+                && !weapon.CurrentUsageItem.IsConsumable
+                && weapon.CurrentUsageItem.IsMeleeWeapon
+                && collisionData.StrikeType == (int)StrikeType.Thrust
+                && collisionData.DamageType == (int)DamageTypes.Pierce
+                && !weapon.IsAnyConsumable())
             {
                 finalDamage *= 1.85f; // 85% bonus dmg against horses
             }
@@ -50,7 +61,15 @@ internal class CrpgAgentApplyDamageModel : DefaultAgentApplyDamageModel
         return finalDamage;
     }
 
-    public override void CalculateCollisionStunMultipliers(Agent attackerAgent, Agent defenderAgent, bool isAlternativeAttack, CombatCollisionResult collisionResult, WeaponComponentData attackerWeapon, WeaponComponentData defenderWeapon, out float attackerStunMultiplier, out float defenderStunMultiplier)
+    public override void CalculateCollisionStunMultipliers(
+        Agent attackerAgent,
+        Agent defenderAgent,
+        bool isAlternativeAttack,
+        CombatCollisionResult collisionResult,
+        WeaponComponentData attackerWeapon,
+        WeaponComponentData defenderWeapon,
+        out float attackerStunMultiplier,
+        out float defenderStunMultiplier)
     {
         attackerStunMultiplier = 1f;
         if (collisionResult == CombatCollisionResult.Blocked && defenderAgent.WieldedOffhandWeapon.IsShield())
@@ -69,12 +88,18 @@ internal class CrpgAgentApplyDamageModel : DefaultAgentApplyDamageModel
         defenderStunMultiplier = 1f;
     }
 
-    public override bool DecideMountRearedByBlow(Agent attackerAgent, Agent victimAgent, in AttackCollisionData collisionData, WeaponComponentData attackerWeapon, in Blow blow)
+    public override bool DecideMountRearedByBlow(
+        Agent attackerAgent,
+        Agent victimAgent,
+        in AttackCollisionData collisionData,
+        WeaponComponentData? attackerWeapon,
+        in Blow blow)
     {
         float damageMultiplierOfCombatDifficulty = Mission.Current.GetDamageMultiplierOfCombatDifficulty(victimAgent, attackerAgent);
         if (attackerWeapon != null
             && attackerWeapon.WeaponFlags.HasAnyFlag(WeaponFlags.WideGrip)
-            && blow.StrikeType == StrikeType.Thrust && collisionData.ThrustTipHit
+            && blow.StrikeType == StrikeType.Thrust
+            && collisionData.ThrustTipHit
             && blow.DamageType == DamageTypes.Pierce
             && attackerAgent != null
             && !attackerAgent.HasMount
@@ -83,7 +108,8 @@ internal class CrpgAgentApplyDamageModel : DefaultAgentApplyDamageModel
             && victimAgent.MovementVelocity.y > 0.1f
             && Vec3.DotProduct(blow.Direction, victimAgent.Frame.rotation.f) < -0.35f)
         {
-            return (float)collisionData.InflictedDamage >= ManagedParameters.Instance.GetManagedParameter(ManagedParametersEnum.MakesRearAttackDamageThreshold) * damageMultiplierOfCombatDifficulty;
+            float rearThreshold = ManagedParameters.Instance.GetManagedParameter(ManagedParametersEnum.MakesRearAttackDamageThreshold);
+            return collisionData.InflictedDamage >= rearThreshold * damageMultiplierOfCombatDifficulty;
         }
 
         return false;
