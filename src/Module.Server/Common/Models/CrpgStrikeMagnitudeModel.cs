b26e01dbe69eddef28b7aa502d2331ab0459d569
Namidaka <worldofmaps@gmail.com>
2022-11-02 20:52:38 +0100

    reduced bonus from speed  to damage on horse back

--- src/Module.Server/Common/Models/CrpgStrikeMagnitudeModel.cs
+++ src/Module.Server/Common/Models/CrpgStrikeMagnitudeModel.cs
@@ -17,6 +17,42 @@ internal class CrpgStrikeMagnitudeModel : MultiplayerStrikeMagnitudeModel
         _constants = constants;
     }
 
+    public override float CalculateStrikeMagnitudeForSwing(BasicCharacterObject attackerCharacter, BasicCharacterObject attackerCaptainCharacter, float swingSpeed, float impactPointAsPercent, float weaponWeight, WeaponComponentData weaponUsageComponent, float weaponLength, float weaponInertia, float weaponCoM, float extraLinearSpeed, bool doesAttackerHaveMount)
+    {
+        float num = weaponLength * impactPointAsPercent - weaponCoM;
+        float num2 = swingSpeed * (0.5f + weaponCoM) + extraLinearSpeed;
+        float num3 = 0.5f * weaponWeight * num2 * num2;
+        float num4 = 0.5f * weaponInertia * swingSpeed * swingSpeed;
+        float num5 = num3 + num4;
+        float num6 = (num2 + swingSpeed * num) / (1f / weaponWeight + num * num / weaponInertia);
+        float num7 = num2 - num6 / weaponWeight;
+        float num8 = swingSpeed - num6 * num / weaponInertia;
+        float num9 = 0.5f * weaponWeight * num7 * num7;
+        float num10 = 0.5f * weaponInertia * num8 * num8;
+        float num11 = num9 + num10;
+        float num12 = num5 - num11 + 0.5f;
+        return 0.067f * num12;
+    }
+
+    public override float CalculateStrikeMagnitudeForThrust(BasicCharacterObject attackerCharacter, BasicCharacterObject attackerCaptainCharacter, float thrustWeaponSpeed, float weaponWeight, WeaponComponentData weaponUsageComponent, float extraLinearSpeed, bool doesAttackerHaveMount, bool isThrown = false)
+    {
+        float num = doesAttackerHaveMount ?
+            thrustWeaponSpeed + (float)Math.Pow(extraLinearSpeed, 0.7f) :
+            thrustWeaponSpeed + extraLinearSpeed;
+        if (num > 0f)
+        {
+            if (!isThrown)
+            {
+                weaponWeight += 2.5f;
+            }
+
+            float num2 = 0.5f * weaponWeight * num * num;
+            return 0.125f * num2;
+        }
+
+        return 0f;
+    }
+
     public override float GetBluntDamageFactorByDamageType(DamageTypes damageType)
     {
         float result = 0f;
