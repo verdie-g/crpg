5e8c3c0d47a69c77921e1a3dccfb3c96fa4411d3
Grégoire Verdier <g.verdier@criteo.com>
2022-11-04 13:33:26 +0100

    Rename variables

--- src/Module.Server/Common/Models/CrpgStrikeMagnitudeModel.cs
+++ src/Module.Server/Common/Models/CrpgStrikeMagnitudeModel.cs
@@ -20,53 +20,58 @@ internal class CrpgStrikeMagnitudeModel : MultiplayerStrikeMagnitudeModel
     public override float CalculateStrikeMagnitudeForSwing(
         BasicCharacterObject attackerCharacter,
         BasicCharacterObject attackerCaptainCharacter,
-        float swingSpeed, float impactPointAsPercent,
-        float weaponWeight, WeaponComponentData weaponUsageComponent,
+        float swingSpeed,
+        float impactPointAsPercent,
+        float weaponWeight,
+        WeaponComponentData weaponUsageComponent,
         float weaponLength,
         float weaponInertia,
-        float weaponCoM,
+        float weaponCenterOfMass,
         float extraLinearSpeed,
         bool doesAttackerHaveMount)
     {
-        float num = weaponLength * impactPointAsPercent - weaponCoM;
-        float num2 = swingSpeed * (0.5f + weaponCoM) + extraLinearSpeed;
-        float num3 = 0.5f * weaponWeight * num2 * num2;
-        float num4 = 0.5f * weaponInertia * swingSpeed * swingSpeed;
-        float num5 = num3 + num4;
-        float num6 = (num2 + swingSpeed * num) / (1f / weaponWeight + num * num / weaponInertia);
-        float num7 = num2 - num6 / weaponWeight;
+        float centerOfMassSpeed = swingSpeed * (0.5f + weaponCenterOfMass) + extraLinearSpeed;
+        float translationalKineticEnergy1 = 0.5f * weaponWeight * centerOfMassSpeed * centerOfMassSpeed;
+        float rotationalKineticEnergy1 = 0.5f * weaponInertia * swingSpeed * swingSpeed;
+        float totalKineticEnergy1 = translationalKineticEnergy1 + rotationalKineticEnergy1;
+
+        float num = weaponLength * impactPointAsPercent - weaponCenterOfMass; // distance to CM?
+        float num6 = (centerOfMassSpeed + swingSpeed * num) / (1f / weaponWeight + num * num / weaponInertia);
+        float num7 = centerOfMassSpeed - num6 / weaponWeight;
         float num8 = swingSpeed - num6 * num / weaponInertia;
-        float num9 = 0.5f * weaponWeight * num7 * num7;
-        float num10 = 0.5f * weaponInertia * num8 * num8;
-        float num11 = num9 + num10;
-        float num12 = num5 - num11 + 0.5f;
+        float translationalKineticEnergy2 = 0.5f * weaponWeight * num7 * num7;
+        float rotationalKineticEnergy2 = 0.5f * weaponInertia * num8 * num8;
+        float totalKineticEnergy2 = translationalKineticEnergy2 + rotationalKineticEnergy2;
+
+        float num12 = totalKineticEnergy1 - totalKineticEnergy2 + 0.5f;
         return 0.067f * num12;
     }
 
     public override float CalculateStrikeMagnitudeForThrust(
         BasicCharacterObject attackerCharacter,
         BasicCharacterObject attackerCaptainCharacter,
-        float thrustWeaponSpeed, float weaponWeight,
+        float thrustWeaponSpeed,
+        float weaponWeight,
         WeaponComponentData weaponUsageComponent,
         float extraLinearSpeed,
         bool doesAttackerHaveMount,
         bool isThrown = false)
     {
-        float num = doesAttackerHaveMount
+        float speed = doesAttackerHaveMount
             ? thrustWeaponSpeed + (float)Math.Pow(extraLinearSpeed, 0.7f)
             : thrustWeaponSpeed + extraLinearSpeed;
-        if (num > 0f)
+        if (speed <= 0f)
         {
-            if (!isThrown)
-            {
-                weaponWeight += 2.5f;
-            }
-
-            float num2 = 0.5f * weaponWeight * num * num;
-            return 0.125f * num2;
+            return 0;
         }
 
-        return 0f;
+        if (!isThrown) // Copied from native.
+        {
+            weaponWeight += 2.5f;
+        }
+
+        float translationalKineticEnergy = 0.5f * weaponWeight * speed * speed;
+        return 0.125f * translationalKineticEnergy;
     }
 
     public override float GetBluntDamageFactorByDamageType(DamageTypes damageType)
