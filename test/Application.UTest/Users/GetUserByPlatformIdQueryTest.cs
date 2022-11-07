5419bc2653f621a6aca709406a0e0cc5f2e46c02
Valery Sinevich <zohan33333@gmail.com>
2022-11-07 20:15:26 +0000

    fix unit

--- test/Application.UTest/Users/GetUserByPlatformIdQueryTest.cs
+++ test/Application.UTest/Users/GetUserByPlatformIdQueryTest.cs
@@ -8,18 +8,6 @@ namespace Crpg.Application.UTest.Users;
 public class GetUserByPlatformIdQueryTest : TestBase
 {
     [Test]
-    public async Task ShouldReturnErrorIfUserDoesNotExist()
-    {
-        GetUserByPlatformIdQuery.Handler handler = new(ActDb, Mapper);
-        var result = await handler.Handle(new GetUserByPlatformIdQuery
-        {
-            Platform = Platform.Steam,
-            PlatformUserId = "1234",
-        }, CancellationToken.None);
-        Assert.AreEqual(ErrorCode.UserNotFound, result.Errors![0].Code);
-    }
-
-    [Test]
     public async Task TestWhenUserExists()
     {
         User user = new()
