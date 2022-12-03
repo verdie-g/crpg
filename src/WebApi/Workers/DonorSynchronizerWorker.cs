63b9091f6c5dca4b7ca10f7a29ad5ecc594321ec
verdie-g <gregoire.verdier@gmail.com>
2022-12-03 20:31:42 +0100

    webapi: support Afdian along Patreon for chinese players

--- src/WebApi/Workers/DonorSynchronizerWorker.cs
+++ src/WebApi/Workers/DonorSynchronizerWorker.cs
@@ -1,4 +1,8 @@
 ﻿using System.Net.Http.Headers;
+using System.Net.Mime;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using Crpg.Application.Users.Commands;
 using MediatR;
@@ -7,7 +11,8 @@ namespace Crpg.WebApi.Workers;
 
 internal class DonorSynchronizerWorker : BackgroundService
 {
-    private const int MinAmountCentsForRewards = 500;
+    private const int MinPatreonAmountCentsForRewards = 500;
+    private const int MinAfdianAmountYuanForRewards = 25;
 
     private static readonly ILogger Logger = Logging.LoggerFactory.CreateLogger<DonorSynchronizerWorker>();
 
@@ -22,47 +27,16 @@ internal class DonorSynchronizerWorker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken cancellationToken)
     {
-        string? patreonAccessToken = _configuration.GetValue<string>("Patreon:AccessToken");
-        if (patreonAccessToken == null)
-        {
-            Logger.LogInformation("No Patreon access token was provided. Disabling the donor synchronization");
-            return;
-        }
-
-        int campaignId = _configuration.GetValue<int>("Patreon:CampaignId");
-        HttpClient client = new()
-        {
-            BaseAddress = new Uri("https://www.patreon.com/api/oauth2/v2/"),
-        };
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", patreonAccessToken);
-
         while (!cancellationToken.IsCancellationRequested)
         {
             try
             {
                 using var scope = _serviceScopeFactory.CreateScope();
 
-                string uri = $"campaigns/{campaignId}/members?fields%5Bmember%5D=currently_entitled_amount_cents,note&page%5Bcount%5D=1000";
-                var res = await client.GetFromJsonAsync<PatreonResponse<PatreonCampaignMember>>(uri, cancellationToken);
+                var patreonDonors = await GetPatreonDonorsAsync(cancellationToken);
+                var afdianDonors = await GetAfdianDonorsAsync(cancellationToken);
 
-                List<string> steamIds = new();
-                foreach (var member in res!.Data)
-                {
-                    if (member.Attributes.CurrentlyEntitledAmountCents < MinAmountCentsForRewards * 0.95) // Allow a little margin.
-                    {
-                        continue;
-                    }
-
-                    string[] noteLines = member.Attributes.Note.Split('\n', StringSplitOptions.RemoveEmptyEntries);
-                    foreach (string noteLine in noteLines)
-                    {
-                        string[] noteParts = noteLine.Split(':', StringSplitOptions.RemoveEmptyEntries);
-                        if (noteParts.Length == 2 && noteParts[0] == "steam")
-                        {
-                            steamIds.Add(noteParts[1]);
-                        }
-                    }
-                }
+                string[] steamIds = patreonDonors.Concat(afdianDonors).ToArray();
 
                 var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
                 await mediator.Send(new UpdateUserDonorsCommand { PlatformUserIds = steamIds }, cancellationToken);
@@ -74,6 +48,95 @@ internal class DonorSynchronizerWorker : BackgroundService
 
             await Task.Delay(TimeSpan.FromHours(1), cancellationToken);
         }
+    }
+
+    private async Task<IEnumerable<string>> GetPatreonDonorsAsync(CancellationToken cancellationToken)
+    {
+        string? patreonAccessToken = _configuration.GetValue<string>("Patreon:AccessToken");
+        if (patreonAccessToken == null)
+        {
+            Logger.LogInformation("No Patreon access token was provided. Skipping the donor synchronization");
+            return Array.Empty<string>();
+        }
+
+        int campaignId = _configuration.GetValue<int>("Patreon:CampaignId");
+        using HttpClient client = new() { BaseAddress = new Uri("https://www.patreon.com/api/oauth2/v2/") };
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", patreonAccessToken);
+
+        string uri = $"campaigns/{campaignId}/members?fields%5Bmember%5D=currently_entitled_amount_cents,note&page%5Bcount%5D=1000";
+        var res = await client.GetFromJsonAsync<PatreonResponse<PatreonCampaignMember>>(uri, cancellationToken);
+
+        List<string> steamIds = new();
+        foreach (var member in res!.Data)
+        {
+            if (member.Attributes.CurrentlyEntitledAmountCents < MinPatreonAmountCentsForRewards * 0.95) // Allow a little margin.
+            {
+                continue;
+            }
+
+            string[] noteLines = member.Attributes.Note.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            foreach (string noteLine in noteLines)
+            {
+                string[] noteParts = noteLine.Split(':', StringSplitOptions.RemoveEmptyEntries);
+                if (noteParts.Length == 2 && noteParts[0] == "steam")
+                {
+                    steamIds.Add(noteParts[1]);
+                }
+            }
+        }
+
+        return steamIds;
+    }
+
+    private async Task<IEnumerable<string>> GetAfdianDonorsAsync(CancellationToken cancellationToken)
+    {
+        string? accessToken = _configuration.GetValue<string>("Afdian:AccessToken");
+        string? userId = _configuration.GetValue<string>("Afdian:UserId");
+        if (accessToken == null || userId == null)
+        {
+            Logger.LogInformation("No Afdian access token was provided. Skipping the donor synchronization");
+            return Array.Empty<string>();
+        }
+
+        Dictionary<string, string> afdianToSteamIds = new(StringComparer.Ordinal)
+        {
+            ["1a4e438e725f11edacfc52540025c377"] = "76561198136476759",
+            ["651ee3be726411ed9b5f52540025c377"] = "76561198835355589",
+            ["956ba96c725f11ed846952540025c377"] = "76561198277953194",
+        };
+
+        using HttpClient httpClient = new() { BaseAddress = new Uri("https://afdian.net/"), };
+
+        const string reqParams = "{\"page\":1}";
+        long timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        string signature = $"{accessToken}params{reqParams}ts{timestamp}user_id{userId}";
+        string sign = Convert.ToHexString(MD5.HashData(Encoding.UTF8.GetBytes(signature))).ToLowerInvariant();
+        string reqContentStr = $"{{\"user_id\":\"{userId}\",\"params\":\"{reqParams.Replace("\"", "\\\"")}\",\"ts\":{timestamp},\"sign\":\"{sign}\"}}";
+        StringContent reqContent = new(reqContentStr, Encoding.UTF8, MediaTypeNames.Application.Json);
+        using var res = await httpClient.PostAsync("api/open/query-sponsor", reqContent, cancellationToken);
+        string resContent = await res.Content.ReadAsStringAsync(cancellationToken);
+        var sponsorsRes = JsonSerializer.Deserialize<AfdianResponse<AfdianSponsor>>(resContent, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+        })!;
+
+        List<string> steamIds = new();
+        foreach (var sponsor in sponsorsRes.Data.List)
+        {
+            if (sponsor.CurrentPlan == null || float.Parse(sponsor.CurrentPlan.Price) < MinAfdianAmountYuanForRewards * 0.95) // Allow a little margin.
+            {
+                continue;
+            }
+
+            if (!afdianToSteamIds.TryGetValue(sponsor.User.UserId, out string? steamId))
+            {
+                continue;
+            }
+
+            steamIds.Add(steamId);
+        }
+
+        return steamIds;
     }
 
     private class PatreonCampaignMember
@@ -93,5 +156,114 @@ internal class DonorSynchronizerWorker : BackgroundService
         public Guid Id { get; set; }
         public T Attributes { get; set; } = default!;
         public string Type { get; set; } = string.Empty;
+    }
+
+    private class AfdianResponse<T>
+    {
+        public int Ec { get; set; }
+        public string Em { get; set; } = string.Empty;
+        public AfdianResponseData<T> Data { get; set; } = default!;
+    }
+
+    private class AfdianResponseData<T>
+    {
+        [JsonPropertyName("total_count")]
+        public int TotalCount { get; set; }
+        [JsonPropertyName("total_page")]
+        public int TotalPage { get; set; }
+        public T[] List { get; set; } = Array.Empty<T>();
+    }
+
+    private class AfdianSponsor
+    {
+        [JsonPropertyName("sponsor_plans")]
+        public AfdianSponsorPlan[] SponsorPlans { get; set; } = Array.Empty<AfdianSponsorPlan>();
+        [JsonPropertyName("current_plan")]
+        public AfdianSponsorPlan? CurrentPlan { get; set; }
+        [JsonPropertyName("all_sum_amount")]
+        public string AllSumAmount { get; set; } = string.Empty;
+        [JsonPropertyName("first_pay_time")]
+        public long FirstPayTime { get; set; }
+        [JsonPropertyName("last_pay_time")]
+        public long LastPayTime { get; set; }
+        [JsonPropertyName("user")]
+        public AfdianUser User { get; set; } = default!;
+    }
+
+    private class AfdianSponsorPlan
+    {
+        [JsonPropertyName("plan_id")]
+        public string PlanId { get; set; } = string.Empty;
+        [JsonPropertyName("rank")]
+        public int Rank { get; set; }
+        [JsonPropertyName("user_id")]
+        public string UserId { get; set; } = string.Empty;
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+        [JsonPropertyName("pic")]
+        public string Pic { get; set; } = string.Empty;
+        [JsonPropertyName("desc")]
+        public string Desc { get; set; } = string.Empty;
+        [JsonPropertyName("price")]
+        public string Price { get; set; } = string.Empty;
+        [JsonPropertyName("update_time")]
+        public int UpdateTime { get; set; }
+        // [JsonPropertyName("timing")]
+        // public AfdianTiming Timing { get; set; }
+        [JsonPropertyName("pay_month")]
+        public int PayMonth { get; set; }
+        [JsonPropertyName("show_price")]
+        public string ShowPrice { get; set; } = string.Empty;
+        [JsonPropertyName("show_price_after_adjust")]
+        public string ShowPriceAfterAdjust { get; set; } = string.Empty;
+        [JsonPropertyName("has_coupon")]
+        public int HasCoupon { get; set; }
+        // [JsonPropertyName("coupon")]
+        // public List<AfdianCoupon> Coupon { get; set; }
+        [JsonPropertyName("favorable_price")]
+        public int FavorablePrice { get; set; }
+        [JsonPropertyName("independent")]
+        public int Independent { get; set; }
+        [JsonPropertyName("permanent")]
+        public int Permanent { get; set; }
+        [JsonPropertyName("can_buy_hide")]
+        public int CanBuyHide { get; set; }
+        [JsonPropertyName("need_address")]
+        public int NeedAddress { get; set; }
+        [JsonPropertyName("product_type")]
+        public int ProductType { get; set; }
+        [JsonPropertyName("sale_limit_count")]
+        public int SaleLimitCount { get; set; }
+        [JsonPropertyName("need_invite_code")]
+        public bool NeedInviteCode { get; set; }
+        [JsonPropertyName("bundle_stock")]
+        public int BundleStock { get; set; }
+        [JsonPropertyName("bundle_sku_select_count")]
+        public int BundleSkuSelectCount { get; set; }
+        // [JsonPropertyName("config")]
+        // public AfdianConfig Config { get; set; }
+        [JsonPropertyName("has_plan_config")]
+        public int HasPlanConfig { get; set; }
+        [JsonPropertyName("expire_time")]
+        public int ExpireTime { get; set; }
+        // [JsonPropertyName("sku_processed")]
+        // public AfdianSkuProcessed[] SkuProcessed
+    }
+
+    private class AfdianUser
+    {
+        [JsonPropertyName("user_id")]
+        public string UserId { get; set; } = string.Empty;
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("avatar")]
+        public string Avatar { get; set; } = string.Empty;
+
+        [JsonPropertyName("user_private_id")]
+        public string UserPrivateId { get; set; } = string.Empty;
     }
 }
