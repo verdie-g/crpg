bdfe5bbe34d11723c1398cd9cbf646164c22d3d2
verdie-g <gregoire.verdier@gmail.com>
2022-12-13 21:58:20 +0100

    mod: add missing balancing file

---
+++ src/Module.Server/Balancing/CrpgCharacterRatingExtension.cs
@@ -0,0 +1,11 @@
+﻿using Crpg.Module.Api.Models.Characters;
+
+namespace Crpg.Module.Balancing;
+
+internal static class CrpgCharacterRatingExtension
+{
+    public static float GetWorkingRating(this CrpgCharacterRating rating)
+    {
+        return rating.Value - 2 * rating.Deviation;
+    }
+}
