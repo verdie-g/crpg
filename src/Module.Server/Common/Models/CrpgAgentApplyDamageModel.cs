1ef666947fbbe9436d339a94cb71888b93eb5cf5
Namidaka <worldofmaps@gmail.com>
2022-12-16 23:54:11 +0100

    bonus against shields

--- src/Module.Server/Common/Models/CrpgAgentApplyDamageModel.cs
+++ src/Module.Server/Common/Models/CrpgAgentApplyDamageModel.cs
@@ -34,6 +34,14 @@ internal class CrpgAgentApplyDamageModel : DefaultAgentApplyDamageModel
             }
 
             finalDamage /= MathHelper.RecursivePolynomialFunctionOfDegree2(shieldSkill, _constants.DurabilityFactorForShieldRecursiveCoefs);
+            if (weapon.CurrentUsageItem.WeaponFlags.HasAnyFlag(WeaponFlags.BonusAgainstShield))
+            {
+                // this bonus is on top of the native x2 in MissionCombatMechanicsHelper
+                // so the final bonus is 3.5. We do this instead of nerfing the impact of shield skill so shield can stay virtually unbreakable against sword.
+                // it is the same logic as arrows not dealing a lot of damage to horse but spears dealing extra damage to horses
+                // As we want archer to fear cavs and cavs to fear spears, we want swords to fear shielders and shielders to fear axes.
+                finalDamage *= 1.75f;
+            }
         }
 
         if (weapon.IsEmpty)
