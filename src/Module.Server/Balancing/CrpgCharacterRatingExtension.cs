a5488998101451bee7e44fa30e9e4908c9af73e0
Namidaka <Namidaka@DESKTOP-JBI8D76>
2022-12-30 14:06:51 +0100

    .

--- src/Module.Server/Balancing/CrpgCharacterRatingExtension.cs
+++ src/Module.Server/Balancing/CrpgCharacterRatingExtension.cs
@@ -6,6 +6,6 @@ internal static class CrpgCharacterRatingExtension
 {
     public static float GetWorkingRating(this CrpgCharacterRating rating)
     {
-        return 0.0025f * (float)Math.Pow(rating.Value - 2* rating.Deviation, 1.5f);
+        return 0.0025f * (float)Math.Pow(rating.Value - 2 * rating.Deviation, 1.5f);
     }
 }
