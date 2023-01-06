f215be85d922439932d3a5b84b2924c11e49f026
verdie-g <gregoire.verdier@gmail.com>
2023-01-06 19:24:58 +0100

    mod: add method ReflectionHelper.RaiseEvent
    It's useful to investigate issues.

--- src/Module.Server/Helpers/ReflectionHelper.cs
+++ src/Module.Server/Helpers/ReflectionHelper.cs
@@ -9,8 +9,8 @@ internal static class ReflectionHelper
 {
     public static object GetField(object instance, string field) => GetFieldInfo(instance, field).GetValue(instance);
     public static void SetField(object instance, string field, object value) => GetFieldInfo(instance, field).SetValue(instance, value);
-    public static object GetProperty(object instance, string field) => GetPropertyInfo(instance, field)!.GetValue(instance);
-    public static void SetProperty(object instance, string field, object value) => GetPropertyInfo(instance, field)!.SetValue(instance, value, null);
+    public static object GetProperty(object instance, string prop) => GetPropertyInfo(instance, prop)!.GetValue(instance);
+    public static void SetProperty(object instance, string prop, object value) => GetPropertyInfo(instance, prop)!.SetValue(instance, value, null);
 
     public static object InvokeMethod(object instance, string method, object[] parameters)
     {
@@ -18,6 +18,12 @@ internal static class ReflectionHelper
             .GetType()
             .GetMethod(method, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)!
             .Invoke(instance, parameters);
+    }
+
+    public static void RaiseEvent(object instance, string evt, object[] parameters)
+    {
+        var raiseMethod = GetEventInfo(instance, evt).GetRaiseMethod();
+        raiseMethod?.Invoke(instance, parameters);
     }
 
     private static FieldInfo GetFieldInfo(object instance, string field)
@@ -52,5 +58,16 @@ internal static class ReflectionHelper
         }
 
         throw new ArgumentException($"Property {prop} not found in {instance.GetType()}");
+    }
+
+    private static EventInfo GetEventInfo(object instance, string evt)
+    {
+        var e = instance.GetType().GetEvent(evt);
+        if (e == null)
+        {
+            throw new ArgumentException($"Event {evt} not found in {instance.GetType()}");
+        }
+
+        return e;
     }
 }
