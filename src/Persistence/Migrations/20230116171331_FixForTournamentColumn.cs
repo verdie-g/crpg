efd87afc96a7a99d78f87cc43542c1efd5578e9e
verdie-g <gregoire.verdier@gmail.com>
2023-01-16 18:14:03 +0100

    webapi: fix character tournament column

---
+++ src/Persistence/Migrations/20230116171331_FixForTournamentColumn.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Crpg.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class FixForTournamentColumn : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.RenameColumn(
+            name: "tournament",
+            table: "characters",
+            newName: "for_tournament");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.RenameColumn(
+            name: "for_tournament",
+            table: "characters",
+            newName: "tournament");
+    }
+}
