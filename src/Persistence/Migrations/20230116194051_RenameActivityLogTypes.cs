7f1ef9287c115b27008ac4c170c302e4d708e41f
verdie-g <gregoire.verdier@gmail.com>
2023-01-17 22:24:50 +0100

    mod: fix activity log enum migration

--- src/Persistence/Migrations/20230116194051_RenameActivityLogTypes.cs
+++ src/Persistence/Migrations/20230116194051_RenameActivityLogTypes.cs
@@ -8,16 +8,26 @@ public partial class RenameActivityLogTypes : Migration
     /// <inheritdoc />
     protected override void Up(MigrationBuilder migrationBuilder)
     {
-        migrationBuilder.AlterDatabase()
-            .Annotation("Npgsql:Enum:activity_log_type", "user_created,user_deleted,user_renamed,item_bought,item_sold,character_created,character_deleted,character_respecialized,character_retired,server_joined,chat_message_sent,team_hit")
-            .OldAnnotation("Npgsql:Enum:activity_log_type", "user_creation,user_deletion,user_server_join,user_renamed,item_bought,item_sold,character_creation,character_deletion,character_respecialization,character_retirement,chat_message,team_hit");
+        migrationBuilder.Sql("ALTER TYPE activity_log_type RENAME VALUE 'user_creation' TO 'user_created';");
+        migrationBuilder.Sql("ALTER TYPE activity_log_type RENAME VALUE 'user_deletion' TO 'user_deleted';");
+        migrationBuilder.Sql("ALTER TYPE activity_log_type RENAME VALUE 'user_server_join' TO 'server_joined';");
+        migrationBuilder.Sql("ALTER TYPE activity_log_type RENAME VALUE 'character_creation' TO 'character_created';");
+        migrationBuilder.Sql("ALTER TYPE activity_log_type RENAME VALUE 'character_deletion' TO 'character_deleted';");
+        migrationBuilder.Sql("ALTER TYPE activity_log_type RENAME VALUE 'character_respecialization' TO 'character_respecialized';");
+        migrationBuilder.Sql("ALTER TYPE activity_log_type RENAME VALUE 'character_retirement' TO 'character_retired';");
+        migrationBuilder.Sql("ALTER TYPE activity_log_type RENAME VALUE 'chat_message' TO 'chat_message_sent';");
     }
 
     /// <inheritdoc />
     protected override void Down(MigrationBuilder migrationBuilder)
     {
-        migrationBuilder.AlterDatabase()
-            .Annotation("Npgsql:Enum:activity_log_type", "user_creation,user_deletion,user_server_join,user_renamed,item_bought,item_sold,character_creation,character_deletion,character_respecialization,character_retirement,chat_message,team_hit")
-            .OldAnnotation("Npgsql:Enum:activity_log_type", "user_created,user_deleted,user_renamed,item_bought,item_sold,character_created,character_deleted,character_respecialized,character_retired,server_joined,chat_message_sent,team_hit");
+        migrationBuilder.Sql("ALTER TYPE activity_log_type RENAME VALUE 'user_created' TO 'user_creation';");
+        migrationBuilder.Sql("ALTER TYPE activity_log_type RENAME VALUE 'user_deleted' TO 'user_deletion';");
+        migrationBuilder.Sql("ALTER TYPE activity_log_type RENAME VALUE 'server_joined' TO 'user_server_join';");
+        migrationBuilder.Sql("ALTER TYPE activity_log_type RENAME VALUE 'character_created' TO 'character_creation';");
+        migrationBuilder.Sql("ALTER TYPE activity_log_type RENAME VALUE 'character_deleted' TO 'character_deletion';");
+        migrationBuilder.Sql("ALTER TYPE activity_log_type RENAME VALUE 'character_respecialized' TO 'character_respecialization';");
+        migrationBuilder.Sql("ALTER TYPE activity_log_type RENAME VALUE 'character_retired' TO 'character_retirement';");
+        migrationBuilder.Sql("ALTER TYPE activity_log_type RENAME VALUE 'chat_message_sent' TO 'chat_message';");
     }
 }
