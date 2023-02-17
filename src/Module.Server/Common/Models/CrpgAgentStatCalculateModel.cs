d3f7eac326c34976680c8ef2a5c127bb6f1267ce
Namidaka <Namidaka@DESKTOP-JBI8D76>
2023-02-17 17:54:06 +0100

    .

--- src/Module.Server/Common/Models/CrpgAgentStatCalculateModel.cs
+++ src/Module.Server/Common/Models/CrpgAgentStatCalculateModel.cs
@@ -309,7 +309,7 @@ internal class CrpgAgentStatCalculateModel : AgentStatCalculateModel
             if (agent.HasMount && !equippedItem.IsRangedWeapon)
             {
                 // SwingSpeed Nerf on Horseback
-                float swingSpeedFactor = 1f / Math.Max(equippedItem.WeaponLength / 120f, 1f);
+                float swingSpeedFactor = 1f / Math.Max(equippedItem.WeaponLength / 140f, 1f);
                 props.SwingSpeedMultiplier *= HasSwingDamage(primaryItem) ? swingSpeedFactor : 1f;
                 // Thrustspeed Nerf on Horseback
                 props.ThrustOrRangedReadySpeedMultiplier *= 0.84f;
