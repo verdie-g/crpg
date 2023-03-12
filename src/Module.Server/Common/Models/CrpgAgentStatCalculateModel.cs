656a195083fe6ffd1ffb7731bc13958bbaab3abc
Namidaka <Namidaka@DESKTOP-JBI8D76>
2023-03-12 15:58:52 +0100

    last version

--- src/Module.Server/Common/Models/CrpgAgentStatCalculateModel.cs
+++ src/Module.Server/Common/Models/CrpgAgentStatCalculateModel.cs
@@ -439,7 +439,7 @@ internal class CrpgAgentStatCalculateModel : AgentStatCalculateModel
 
     private int MaxWeaponLengthForStrLevel(int strengthSkill)
     {
-        int uncappedMaxWeaponLength = (int)(30 + (strengthSkill - 3) * 12 + Math.Pow((strengthSkill - 3) * 0.13492828, 10));
+        int uncappedMaxWeaponLength = (int)(30 + (strengthSkill - 3) * 9 + Math.Pow((strengthSkill - 3) * 0.14677993f, 12));
         return Math.Min(uncappedMaxWeaponLength, 650);
     }
 
