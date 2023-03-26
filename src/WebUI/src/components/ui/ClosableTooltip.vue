03e200c6c6571d687f835b008c56585de618de90
VALERIY SINEVICH <33551334+or2e@users.noreply.github.com>
2023-03-26 17:36:59 +0300

    webui: warn user when upkeep is too high (#852 fix #751)

---
+++ src/WebUI/src/components/ui/ClosableTooltip.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <VTooltip>
+    <slot />
+    <template #popper="{ hide }">
+      <div class="max-w-xs pr-7">
+        <OButton
+          class="absolute -right-2 -top-2"
+          iconRight="close"
+          rounded
+          size="2xs"
+          variant="secondary"
+          @click="hide"
+        />
+
+        <slot name="popper" />
+      </div>
+    </template>
+  </VTooltip>
+</template>
