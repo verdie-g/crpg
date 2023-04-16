b8dd65adac1c590ac2d6e8e4c14e47df8b248ed0
Grégoire Verdier <g.verdier@criteo.com>
2023-04-16 16:56:36 +0200

    webapi: store authorizations in-memory

---
+++ src/WebApi/Identity/OpenIddictDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Crpg.WebApi.Identity;
+
+internal class OpenIddictDbContext : DbContext
+{
+    public OpenIddictDbContext(DbContextOptions<OpenIddictDbContext> options)
+        : base(options)
+    {
+    }
+}
