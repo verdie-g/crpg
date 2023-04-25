0236fa96a3b0ca3078a630f353afd4559a36230e
Namidaka <Namidaka@DESKTOP-JBI8D76>
2023-04-25 22:41:06 +0200

    Accuracy now depend on damage.

--- src/Module.Server/Common/Models/CrpgAgentStatCalculateModel.cs
+++ src/Module.Server/Common/Models/CrpgAgentStatCalculateModel.cs
@@ -74,10 +74,10 @@ internal class CrpgAgentStatCalculateModel : AgentStatCalculateModel
         {
             WeaponClass.Bow => 1.25f,
             WeaponClass.Crossbow => 0.5f,
-            WeaponClass.Stone => 1f,
-            WeaponClass.ThrowingAxe => 1.55f,
-            WeaponClass.ThrowingKnife => 1.0f,
-            WeaponClass.Javelin => 1.65f,
+            WeaponClass.Stone => (float)Math.Pow(weapon.ThrustDamage / 20f, 1.5f) * 1f,
+            WeaponClass.ThrowingAxe => (float)Math.Pow(weapon.ThrustDamage / 29f, 1.5f) * 1.65f,
+            WeaponClass.ThrowingKnife => (float)Math.Pow(weapon.ThrustDamage / 29f, 1.5f) * 1.0f,
+            WeaponClass.Javelin => (float)Math.Pow(weapon.ThrustDamage / 35f, 1.5f) * 1.65f,
             _ => 1f,
         };
 
