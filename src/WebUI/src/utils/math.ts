6532cf181580c94708b03b352be82f062cb25015
VALERIY SINEVICH <33551334+or2e@users.noreply.github.com>
2023-04-28 11:35:30 +0300

    webui: improve experience bar (#923)

--- src/WebUI/src/utils/math.ts
+++ src/WebUI/src/utils/math.ts
@@ -12,3 +12,9 @@ export const clamp = (num: number, min: number, max: number): number => {
 };
 
 export const roundFLoat = (num: number) => Math.round((num + Number.EPSILON) * 100) / 100;
+
+export const percentOf = (val: number, of: number) => {
+  if (of === 0) return 0;
+
+  return (val / of) * 100;
+};
