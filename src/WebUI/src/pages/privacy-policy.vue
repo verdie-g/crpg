190776d385ccbe01366cb04467080cdbe0ba9340
Valery Sinevich <zohan33333@gmail.com>
2023-05-07 11:27:26 +0000

    tos-ui

--- src/WebUI/src/pages/privacy-policy.vue
+++ src/WebUI/src/pages/privacy-policy.vue
@@ -2,64 +2,72 @@
 definePage({
   meta: {
     layout: 'empty',
+    skipAuth: true,
   },
 });
 </script>
 
 <template>
-  <div class="">
-    <h1>Privacy Policy</h1>
+  <div class="flex w-full items-center justify-center py-12">
+    <div class="mx-auto max-w-3xl rounded-xl border border-border-200 px-6 py-10">
+      <div class="prose prose-invert">
+        <h1 class="text-center">Privacy Policy</h1>
 
-    <p>
-      We respect your privacy and are committed to protecting your personal information. This
-      Privacy Policy explains how we collect, use, and disclose the information you provide to
-      us when using our website for the cRPG multiplayer mod of Mount and Blade II: Bannerlord.
-    </p>
+        <Divider />
 
-    <ol>
-      <li>
-        Information we collect: When you use our website, we collect your user ID from the Steam,
-        EpicGames, or Xbox login provider you use to access the website. We do not collect or store
-        any other personal information, such as your name, address, or email address.
-      </li>
+        <p>
+          We respect your privacy and are committed to protecting your personal information. This
+          Privacy Policy explains how we collect, use, and disclose the information you provide to
+          us when using our website for the cRPG multiplayer mod of Mount and Blade II: Bannerlord.
+        </p>
 
-      <li>
-        Use of information: The user ID we collect is used solely for the purpose of linking your
-        in-game characters with your user account on our website. We do not share or sell your
-        personal information to any third parties.
-      </li>
+        <ol>
+          <li>
+            Information we collect: When you use our website, we collect your user ID from the
+            Steam, EpicGames, or Xbox login provider you use to access the website. We do not
+            collect or store any other personal information, such as your name, address, or email
+            address.
+          </li>
 
-      <li>
-        Cookies: We use cookies on our website to persist user authentication. Cookies are small
-        data files that are placed on your device when you access our website. You can disable
-        cookies in your browser settings if you do not wish to have them stored on your device.
-      </li>
+          <li>
+            Use of information: The user ID we collect is used solely for the purpose of linking
+            your in-game characters with your user account on our website. We do not share or sell
+            your personal information to any third parties.
+          </li>
 
-      <li>
-        Third-party links: Our website may contain links to third-party websites, such as the
-        Steam, EpicGames, or Xbox websites. We are not responsible for the privacy practices or
-        content of these third-party websites.
-      </li>
+          <li>
+            Cookies: We use cookies on our website to persist user authentication. Cookies are small
+            data files that are placed on your device when you access our website. You can disable
+            cookies in your browser settings if you do not wish to have them stored on your device.
+          </li>
 
-      <li>
-        Security: We take reasonable precautions to protect the information we collect from
-        unauthorized access, use, and disclosure. However, no data transmission over the internet
-        can be guaranteed to be 100% secure, and we cannot guarantee the security of your personal
-        information.
-      </li>
+          <li>
+            Third-party links: Our website may contain links to third-party websites, such as the
+            Steam, EpicGames, or Xbox websites. We are not responsible for the privacy practices or
+            content of these third-party websites.
+          </li>
 
-      <li>
-        Children's privacy: Our website is not intended for use by children under the age of 13. We
-        do not knowingly collect personal information from children under the age of 13. If we
-        become aware that we have collected personal information from a child under the age of 13,
-        we will take steps to delete the information as soon as possible.
-      </li>
+          <li>
+            Security: We take reasonable precautions to protect the information we collect from
+            unauthorized access, use, and disclosure. However, no data transmission over the
+            internet can be guaranteed to be 100% secure, and we cannot guarantee the security of
+            your personal information.
+          </li>
 
-      <li>
-        Changes to this Privacy Policy: We reserve the right to change this Privacy Policy at any
-        time without prior notice. Your continued use of our website after any changes to this
-        Privacy Policy constitutes your acceptance of the new Privacy Policy.
-      </li>
-    </ol>
+          <li>
+            Children's privacy: Our website is not intended for use by children under the age of 13.
+            We do not knowingly collect personal information from children under the age of 13. If
+            we become aware that we have collected personal information from a child under the age
+            of 13, we will take steps to delete the information as soon as possible.
+          </li>
+
+          <li>
+            Changes to this Privacy Policy: We reserve the right to change this Privacy Policy at
+            any time without prior notice. Your continued use of our website after any changes to
+            this Privacy Policy constitutes your acceptance of the new Privacy Policy.
+          </li>
+        </ol>
+      </div>
+    </div>
   </div>
 </template>
--- src/WebUI/src/pages/terms-of-service.vue
+++ src/WebUI/src/pages/terms-of-service.vue
@@ -2,66 +2,78 @@
 definePage({
   meta: {
     layout: 'empty',
+    skipAuth: true,
   },
 });
 </script>
 
 <template>
-  <div class="">
-    <h1>Terms of Service</h1>
+  <div class="flex w-full items-center justify-center py-12">
+    <div class="mx-auto max-w-3xl rounded-xl border border-border-200 px-6 py-10">
+      <div class="prose prose-invert">
+        <h1 class="text-center">Terms of Service</h1>
 
-    <p>
-      Welcome to our website for the cRPG multiplayer mod of Mount and Blade II: Bannerlord.
-      Our website provides a user interface to connect players and facilitate communication,
-      and also features Steam, EpicGames, and Xbox logins to link the player in-game.
-    </p>
+        <Divider />
 
-    <ol>
-      <li>
-        Use of our website: By using our website, you agree to the terms and conditions set forth
-        in this document. If you do not agree to these terms, please do not use our website.
-      </li>
+        <p>
+          Welcome to our website for the cRPG multiplayer mod of Mount and Blade II: Bannerlord. Our
+          website provides a user interface to connect players and facilitate communication, and
+          also features Steam, EpicGames, and Xbox logins to link the player in-game.
+        </p>
 
-      <li>
-        User accounts: To access certain features of our website, such as managing your in-game
-        characters, you may be required to create a user account. When creating a user account, you
-        must provide accurate and complete information. You are responsible for maintaining the
-        confidentiality of your account information.
-      </li>
+        <Divider />
 
-      <li>
-        User login: Our website allows users to login using their Steam, EpicGames, or Xbox account.
-        The only information we collect through these logins is the user ID provided by the login
-        provider. We do not collect or store any other sensitive information.
-      </li>
+        <ol>
+          <li>
+            Use of our website: By using our website, you agree to the terms and conditions set
+            forth in this document. If you do not agree to these terms, please do not use our
+            website.
+          </li>
 
-      <li>
-        Code of conduct: Our website is intended to be a friendly and respectful community. We do
-        not tolerate any form of harassment or abusive behavior towards other users. Users who
-        violate this code of conduct may have their account suspended or terminated.
-      </li>
+          <li>
+            User accounts: To access certain features of our website, such as managing your in-game
+            characters, you may be required to create a user account. When creating a user account,
+            you must provide accurate and complete information. You are responsible for maintaining
+            the confidentiality of your account information.
+          </li>
 
-      <li>
-        Limitation of liability: We are not responsible for any damages or losses that may result
-        from your use of our website, including but not limited to loss of data or income. We make
-        no warranties or representations, express or implied, about the accuracy or completeness of
-        the information on our website.
-      </li>
+          <li>
+            User login: Our website allows users to login using their Steam, EpicGames, or Xbox
+            account. The only information we collect through these logins is the user ID provided by
+            the login provider. We do not collect or store any other sensitive information.
+          </li>
 
-      <li>
-        Changes to the Terms of Service: We reserve the right to change these Terms of Service at
-        any time without prior notice. Your continued use of our website after any changes to
-        these Terms of Service constitutes your acceptance of the new Terms of Service.
-      </li>
+          <li>
+            Code of conduct: Our website is intended to be a friendly and respectful community. We
+            do not tolerate any form of harassment or abusive behavior towards other users. Users
+            who violate this code of conduct may have their account suspended or terminated.
+          </li>
 
-      <li>
-        Governing law: These Terms of Service shall be governed by and construed in accordance
-        with the laws of France.
-      </li>
-    </ol>
+          <li>
+            Limitation of liability: We are not responsible for any damages or losses that may
+            result from your use of our website, including but not limited to loss of data or
+            income. We make no warranties or representations, express or implied, about the accuracy
+            or completeness of the information on our website.
+          </li>
 
-    <p>
-      Thank you for using our website, and enjoy playing cRPG in Mount and Blade II: Bannerlord!
-    </p>
+          <li>
+            Changes to the Terms of Service: We reserve the right to change these Terms of Service
+            at any time without prior notice. Your continued use of our website after any changes to
+            these Terms of Service constitutes your acceptance of the new Terms of Service.
+          </li>
+
+          <li>
+            Governing law: These Terms of Service shall be governed by and construed in accordance
+            with the laws of France.
+          </li>
+        </ol>
+
+        <Divider />
+
+        <p>
+          Thank you for using our website, and enjoy playing cRPG in Mount and Blade II: Bannerlord!
+        </p>
+      </div>
+    </div>
   </div>
 </template>
