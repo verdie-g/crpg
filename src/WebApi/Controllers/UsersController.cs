e99aa56e10d139765fef85afcfa867ccb5a6bf30
Namidaka <Namidaka@DESKTOP-JBI8D76>
2023-05-08 15:00:18 +0200

    Wrote a test and renamed endpoit

--- src/WebApi/Controllers/UsersController.cs
+++ src/WebApi/Controllers/UsersController.cs
@@ -216,7 +216,7 @@ public class UsersController : BaseController
     /// <param name="id">Character id.</param>
     /// <returns>The character rating.</returns>
     /// <response code="200">Ok.</response>
-    [HttpGet("self/characters/{id}/ratings")]
+    [HttpGet("self/characters/{id}/rating")]
     public Task<ActionResult<Result<CharacterRatingViewModel>>> GetCharacterRating([FromRoute] int id)
     {
         return ResultToActionAsync(Mediator.Send(new GetUserCharacterRatingQuery
---
+++ test/Application.UTest/Characters/GetUserCharacterRatingQueryTest.cs
@@ -0,0 +1,45 @@
+﻿using Crpg.Application.Characters.Queries;
+using Crpg.Application.Common.Results;
+using Crpg.Domain.Entities.Characters;
+using NUnit.Framework;
+
+namespace Crpg.Application.UTest.Characters;
+
+public class GetUserCharacterRatingQueryTest : TestBase
+{
+    [Test]
+    public async Task ShouldReturnErrorIfCharacterRatingDoesntExist()
+    {
+        GetUserCharacterRatingQuery.Handler handler = new(ActDb, Mapper);
+        var result = await handler.Handle(new GetUserCharacterRatingQuery
+        {
+            CharacterId = 1,
+            UserId = 2,
+        }, CancellationToken.None);
+
+        Assert.That(result.Errors![0].Code, Is.EqualTo(ErrorCode.CharacterNotFound));
+    }
+
+    [Test]
+    public async Task ShouldReturnCharacterRating()
+    {
+        Character character = new()
+        {
+            Name = "toto",
+            UserId = 2,
+            Rating = new(),
+        };
+        ArrangeDb.Characters.Add(character);
+        await ArrangeDb.SaveChangesAsync();
+
+        GetUserCharacterRatingQuery.Handler handler = new(ActDb, Mapper);
+        var result = await handler.Handle(new GetUserCharacterRatingQuery
+        {
+            CharacterId = character.Id,
+            UserId = 2,
+        }, CancellationToken.None);
+
+        Assert.That(result.Errors, Is.Null);
+        Assert.That(result.Data, Is.Not.Null);
+    }
+}
