f29b13a472f1b18003bbc3bd125922c79d1cb197
MrMendo <mrmendo@hotmail.de>
2023-05-11 02:45:42 +0200

    mod: gold compensation for teamhits

---
+++ src/Module.Server/Rewards/CrpgUserUpkeepCompensation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crpg.Module.Api.Models;
+internal class CrpgUserUpkeepCompensation
+{
+    public int RepairCostToCompensate { get; set; }
+    public int CompensatedRepairCost { get; set; }
+    public int TotalRepairCost { get; set; }
+
+    public int GetNetUpkeepCompensation()
+    {
+        return CompensatedRepairCost - RepairCostToCompensate;
+    }
+}
