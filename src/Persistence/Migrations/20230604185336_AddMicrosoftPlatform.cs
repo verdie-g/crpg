7dea3ad7a0ed3a37e2f44bd8568535343ccbe686
verdie-g <gregoire.verdier@gmail.com>
2023-06-04 14:56:58 -0400

    webapi: add microsoft to platform db enum

---
+++ src/Persistence/Migrations/20230604185336_AddMicrosoftPlatform.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Crpg.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class AddMicrosoftPlatform : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterDatabase()
+            .Annotation("Npgsql:Enum:platform", "steam,epic_games,microsoft")
+            .OldAnnotation("Npgsql:Enum:platform", "steam,epic_games");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterDatabase()
+            .Annotation("Npgsql:Enum:platform", "steam,epic_games")
+            .OldAnnotation("Npgsql:Enum:platform", "steam,epic_games,microsoft");
+    }
+}
