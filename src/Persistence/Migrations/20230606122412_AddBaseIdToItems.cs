7890c2fc79faf2f43042cd5c0b026dcc2fe52f55
Namidaka <Namidaka@DESKTOP-JBI8D76>
2023-06-06 14:24:24 +0200

    Migration

---
+++ src/Persistence/Migrations/20230606122412_AddBaseIdToItems.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Crpg.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBaseIdToItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "base_id",
+                table: "items",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "base_id",
+                table: "items");
+        }
+    }
+}
