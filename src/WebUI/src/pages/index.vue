7e392daa8a3535c6b3a51c5320c8db63a6e29457
Valery Sinevich <zohan33333@gmail.com>
2023-06-11 16:32:18 +0000

    webui: main-page-intro

--- src/WebUI/src/pages/index.vue
+++ src/WebUI/src/pages/index.vue
@@ -17,38 +17,38 @@ await loadGameServerStats();
     <Bg bg="background-1.webp" />
 
     <div class="relative flex h-full items-center border border-border-300 text-content-200">
-      <SwitchLanguageDropdown class="absolute right-6 top-6">
-        <template #default="{ shown, locale }">
-          <OButton :variant="shown ? 'transparent-active' : 'transparent'" size="sm">
-            <span class="text-xs font-normal">{{ locale.toUpperCase() }}</span>
-            <div class="flex items-center gap-2.5">
-              <SvgSpriteImg :name="`locale-${locale}`" viewBox="0 0 18 18" class="w-4" />
-              <div class="h-4 w-px select-none bg-border-300"></div>
-              <OIcon
-                icon="chevron-down"
-                size="lg"
-                :rotation="shown ? 180 : 0"
-                class="text-content-400"
-              />
-            </div>
-          </OButton>
-        </template>
-      </SwitchLanguageDropdown>
+      <div class="absolute right-6 top-6 flex items-center gap-6">
+        <OnlinePlayers :gameServerStats="gameServerStats" showLabel />
 
-      <div class="mx-auto space-y-8 md:w-1/2 2xl:w-1/3 2xl:space-y-10">
-        <div class="item-center mb-6 flex select-none justify-center gap-6 md:gap-12">
-          <SvgSpriteImg
-            name="logo-decor"
-            viewBox="0 0 108 10"
-            class="w-16 rotate-180 transform xl:w-20 2xl:w-24"
-          />
-          <SvgSpriteImg name="logo" viewBox="0 0 162 124" class="w-24 xl:w-36 2xl:w-40" />
-          <SvgSpriteImg name="logo-decor" viewBox="0 0 108 10" class="w-16 xl:w-20 2xl:w-24" />
+        <SwitchLanguageDropdown>
+          <template #default="{ shown, locale }">
+            <OButton :variant="shown ? 'transparent-active' : 'transparent'" size="sm">
+              <span class="text-xs font-normal">{{ locale.toUpperCase() }}</span>
+              <div class="flex items-center gap-2.5">
+                <SvgSpriteImg :name="`locale-${locale}`" viewBox="0 0 18 18" class="w-4" />
+                <div class="h-4 w-px select-none bg-border-300"></div>
+                <OIcon
+                  icon="chevron-down"
+                  size="lg"
+                  :rotation="shown ? 180 : 0"
+                  class="text-content-400"
+                />
+              </div>
+            </OButton>
+          </template>
+        </SwitchLanguageDropdown>
+      </div>
+
+      <div class="mx-auto flex flex-col items-center justify-center gap-14 md:w-1/2 2xl:w-1/3">
+        <div class="space-y-6">
+          <div class="item-center flex select-none justify-center gap-6 md:gap-12">
+            <SvgSpriteImg name="logo-decor" viewBox="0 0 108 10" class="w-24 rotate-180" />
+            <SvgSpriteImg name="logo" viewBox="0 0 162 124" class="w-24 xl:w-28 2xl:w-32" />
+            <SvgSpriteImg name="logo-decor" viewBox="0 0 108 10" class="w-24" />
+          </div>
         </div>
 
-        <Divider />
-
-        <div class="prose prose-invert mx-auto text-center">
+        <div class="prose prose-invert text-center">
           <i18n-t keypath="homePage.intro" tag="p" scope="global">
             <template #link>
               <a
@@ -64,10 +64,15 @@ await loadGameServerStats();
           <p>
             {{ $t('homePage.description') }}
           </p>
-        </div>
 
-        <div class="flex justify-center">
-          <OnlinePlayers :gameServerStats="gameServerStats" showLabel />
+          <iframe
+            class="mx-auto block aspect-video w-full w-full rounded-lg lg:w-3/4 xl:w-2/3"
+            src="https://www.youtube-nocookie.com/embed/MZx56LD0o4c"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          />
         </div>
 
         <div class="flex justify-center gap-4">
@@ -75,9 +80,7 @@ await loadGameServerStats();
           <InstallationGuide />
         </div>
 
-        <Divider />
-
-        <Socials class="justify-center" />
+        <Socials class="absolute bottom-6 left-6" />
       </div>
     </div>
   </div>
