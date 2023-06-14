d9576da9f3613317fba769afcbfa2a57a522c536
Namidaka <Namidaka@DESKTOP-JBI8D76>
2023-06-14 20:15:11 +0200

    scoped namespace No nullable disable

--- src/Persistence/Migrations/20230521161426_CompetitiveRatingMigration.cs
+++ src/Persistence/Migrations/20230521161426_CompetitiveRatingMigration.cs
@@ -1,29 +1,26 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
 
-#nullable disable
+namespace Crpg.Persistence.Migrations;
 
-namespace Crpg.Persistence.Migrations
+/// <inheritdoc />
+public partial class CompetitiveRatingMigration : Migration
 {
     /// <inheritdoc />
-    public partial class CompetitiveRatingMigration : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<float>(
-                name: "rating_competitive_rating",
-                table: "characters",
-                type: "real",
-                nullable: false,
-                defaultValue: 0f);
-        }
+        migrationBuilder.AddColumn<float>(
+            name: "rating_competitive_rating",
+            table: "characters",
+            type: "real",
+            nullable: false,
+            defaultValue: 0f);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "rating_competitive_rating",
-                table: "characters");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "rating_competitive_rating",
+            table: "characters");
     }
 }
