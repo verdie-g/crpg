5b948b91fd17f17a5a8c7d3d9709ac665c9a2ec9
namidaka <namidaka@DESKTOP-AHHV998>
2023-06-23 21:31:40 +0200

    rating fix

--- test/Application.UTest/Common/Services/CompetitiveRatingModelTest.cs
+++ test/Application.UTest/Common/Services/CompetitiveRatingModelTest.cs
@@ -7,12 +7,11 @@ namespace Crpg.Application.UTest.Common.Services;
 public class CompetitiveRatingModelTest
 {
     [Test]
-    [Ignore("Broken for now")]
     public void HigherRatingGivesHigherCompetitiveRating()
     {
         CompetitiveRatingModel competitiveRatingModel = new();
         float cr1 = competitiveRatingModel.ComputeCompetitiveRating(
-            new CharacterRating { Value = 100, Deviation = 10 });
+            new CharacterRating { Value = 100, Deviation = 500 });
         float cr2 = competitiveRatingModel.ComputeCompetitiveRating(
             new CharacterRating { Value = 200, Deviation = 10 });
         Assert.That(cr1, Is.LessThan(cr2));
