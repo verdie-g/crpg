74c873036671fbb09c6ff4614ca1b34f46ddcfcf
verdie-g <gregoire.verdier@gmail.com>
2023-06-23 15:00:09 -0400

    webapi: add tests for competitive rating

---
+++ test/Application.UTest/Characters/UpdateEveryCharacterCompetitiveRatingCommandTest.cs
@@ -0,0 +1,25 @@
+﻿using Crpg.Application.Characters.Commands;
+using Crpg.Application.Common.Services;
+using Crpg.Domain.Entities.Characters;
+using Moq;
+using NUnit.Framework;
+
+namespace Crpg.Application.UTest.Characters;
+
+public class UpdateEveryCharacterCompetitiveRatingCommandTest : TestBase
+{
+    [Test]
+    public async Task Basic()
+    {
+        Character character0 = new();
+        Character character1 = new();
+        ArrangeDb.Characters.AddRange(character0, character1);
+        await ArrangeDb.SaveChangesAsync();
+
+        Mock<ICompetitiveRatingModel> competitiveRatingModel = new();
+        UpdateEveryCharacterCompetitiveRatingCommand.Handler handler = new(ActDb, competitiveRatingModel.Object);
+        await handler.Handle(new UpdateEveryCharacterCompetitiveRatingCommand(), CancellationToken.None);
+
+        competitiveRatingModel.Verify(m => m.ComputeCompetitiveRating(It.IsAny<CharacterRating>()), Times.Exactly(2));
+    }
+}
---
+++ test/Application.UTest/Common/Services/CompetitiveRatingModelTest.cs
@@ -0,0 +1,20 @@
+﻿using Crpg.Application.Common.Services;
+using Crpg.Domain.Entities.Characters;
+using NUnit.Framework;
+
+namespace Crpg.Application.UTest.Common.Services;
+
+public class CompetitiveRatingModelTest
+{
+    [Test]
+    [Ignore("Broken for now")]
+    public void HigherRatingGivesHigherCompetitiveRating()
+    {
+        CompetitiveRatingModel competitiveRatingModel = new();
+        float cr1 = competitiveRatingModel.ComputeCompetitiveRating(
+            new CharacterRating { Value = 100, Deviation = 10 });
+        float cr2 = competitiveRatingModel.ComputeCompetitiveRating(
+            new CharacterRating { Value = 200, Deviation = 10 });
+        Assert.That(cr1, Is.LessThan(cr2));
+    }
+}
