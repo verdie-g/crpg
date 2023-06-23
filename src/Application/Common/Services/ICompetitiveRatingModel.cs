703a4d9632e2cbc8e7a0cf7fa77cafc10aaf1dc0
namidaka <47090987+namidaka@users.noreply.github.com>
2023-06-24 00:20:40 +0200

    Update src/Application/Common/Services/ICompetitiveRatingModel.cs
    Co-authored-by: Grégoire <gregoire.verdier@gmail.com>

--- src/Application/Common/Services/ICompetitiveRatingModel.cs
+++ src/Application/Common/Services/ICompetitiveRatingModel.cs
@@ -17,8 +17,8 @@ internal class CompetitiveRatingModel : ICompetitiveRatingModel
     public float ComputeCompetitiveRating(CharacterRating rating)
     {
         double ratingWithUncertainty = 0.01 * (rating.Value - 2 * rating.Deviation);
-        double competitiveRating = ratingWithUncertainty < 0 ?
-                0.03f * -Math.Pow(-ratingWithUncertainty, 3.98)
+        double competitiveRating = ratingWithUncertainty < 0
+            ? 0.03f * -Math.Pow(-ratingWithUncertainty, 3.98)
             : 0.03f * Math.Pow(ratingWithUncertainty, 3.98);
 
         return (float)competitiveRating;
