918a1f0f32a616e9e124f99741b016171d127a67
namidaka <namidaka@DESKTOP-AHHV998>
2023-06-24 02:13:45 +0200

    .

--- src/Module.Server/HarmonyPatches/GetUnsentGameLogsPatch.cs
+++ src/Module.Server/HarmonyPatches/GetUnsentGameLogsPatch.cs
@@ -1,28 +1,23 @@
-﻿using System;
-using HarmonyLib;
-using TaleWorlds.MountAndBlade.DedicatedCustomServer;
-using System.Collections.Generic;
-using System.Linq;
+﻿using TaleWorlds.MountAndBlade.DedicatedCustomServer;
 using TaleWorlds.MountAndBlade.Diamond;
 
-namespace Crpg.Module.HarmonyPatches
-{
-    public static class GetUnsentGameLogsPatch
-    {
-        // This method will run before DedicatedCustomServerSubModule.GetUnsentGameLogs()
-        public static bool Prefix(DedicatedCustomServerSubModule __instance, ref GameLog[] __result)
-        {
-            // Check if GameLogger or GameLogs is null
-            if (__instance.GameLogger == null || __instance.GameLogger.GameLogs == null)
-            {
-                // Return an empty array
-                __result = new GameLog[0];
-                // Don't run the original method
-                return false;
-            }
+namespace Crpg.Module.HarmonyPatches;
 
-            // Run the original method
-            return true;
+public static class GetUnsentGameLogsPatch
+{
+    // This method will run before DedicatedCustomServerSubModule.GetUnsentGameLogs()
+    public static bool Prefix(DedicatedCustomServerSubModule __instance, ref GameLog[] __result)
+    {
+        // Check if GameLogger or GameLogs is null
+        if (__instance.GameLogger == null || __instance.GameLogger.GameLogs == null)
+        {
+            // Return an empty array
+            __result = new GameLog[0];
+            // Don't run the original method
+            return false;
         }
+
+        // Run the original method
+        return true;
     }
 }
