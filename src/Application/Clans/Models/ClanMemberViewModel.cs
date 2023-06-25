e500a49f6da7032d722538bf136b6d6e5547fcdb
namidaka <namidaka@DESKTOP-AHHV998>
2023-06-25 22:46:34 +0200

    format

--- src/Application/Clans/Models/ClanMemberViewModel.cs
+++ src/Application/Clans/Models/ClanMemberViewModel.cs
@@ -9,5 +9,4 @@ public record ClanMemberViewModel : IMapFrom<ClanMember>
     public UserPublicViewModel User { get; init; } = default!;
     public ClanMemberRole Role { get; init; }
     public Clan? Clan { get; set; }
-
 }
