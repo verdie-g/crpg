32896d959190539886a6f489ec431807f4182c8f
Namidaka <namidaka@crpg>
2023-07-09 03:28:38 +0200

    Migration

---
+++ src/Persistence/Migrations/20230709012636_AddResetRatingRestrictionType.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Crpg.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class AddResetRatingRestrictionType : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterDatabase()
+            .Annotation("Npgsql:Enum:restriction_type", "all,join,chat,rating_reset")
+            .OldAnnotation("Npgsql:Enum:restriction_type", "all,join,chat");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterDatabase()
+            .Annotation("Npgsql:Enum:restriction_type", "all,join,chat")
+            .OldAnnotation("Npgsql:Enum:restriction_type", "all,join,chat,rating_reset");
+    }
+}
