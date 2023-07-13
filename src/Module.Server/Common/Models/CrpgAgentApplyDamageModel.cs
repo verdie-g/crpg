08c920712442d9c613233433cf026fddeb3fc3f8
namidaka <47090987+namidaka@users.noreply.github.com>
2023-07-13 05:48:15 +0200

    Update src/Module.Server/Common/Models/CrpgAgentApplyDamageModel.cs
    Co-authored-by: Grégoire <gregoire.verdier@gmail.com>

--- src/Module.Server/Common/Models/CrpgAgentApplyDamageModel.cs
+++ src/Module.Server/Common/Models/CrpgAgentApplyDamageModel.cs
@@ -195,7 +195,7 @@ internal class CrpgAgentApplyDamageModel : MultiplayerAgentApplyDamageModel
         defenderStunMultiplier = 1f;
     }
 
-    // This method should be adjuste once totalenergy uses our strike model to adjust the result when the strike is very bad.
+    // Consider reworking once https://forums.taleworlds.com/index.php?threads/missioncombatmechanicshelper-getdefendcollisionresults-bypass-strikemagnitudecalculationmodel.459379 is fixed
     public override bool DecideCrushedThrough(
         Agent attackerAgent,
         Agent defenderAgent,
