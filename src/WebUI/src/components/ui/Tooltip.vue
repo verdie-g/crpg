cee333dca44901ad3e61a195db1f922e0bed16fe
Valeriy Sinevich <zohan33333@gmail.com>
2023-07-22 20:27:45 +0000

    improve tooltip cmp

--- src/WebUI/src/components/ui/Tooltip.vue
+++ src/WebUI/src/components/ui/Tooltip.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts">
-const { title, description } = defineProps<{
-  title?: string;
-  description?: string;
+const {
+  title,
+  description,
+  disabled = false,
+} = defineProps<{
+  title?: string | null | undefined;
+  description?: string | null | undefined;
+  disabled?: boolean;
 }>();
+
+const isDisabled = computed(
+  () => disabled || title === undefined || title === null || title === ''
+);
 </script>
 
 <template>
-  <VTooltip placement="auto">
+  <VTooltip placement="auto" :disabled="isDisabled">
     <slot />
 
-    <template #popper>
+    <template v-if="!isDisabled" #popper>
       <div class="prose prose-invert space-y-3">
-        <h5 v-if="title" class="text-content-100">{{ title }}</h5>
-        <div v-if="description" v-html="description" class="text-2xs" />
+        <h5 class="text-content-100">
+          {{ title }}
+        </h5>
+        <div
+          v-if="description !== undefined && description !== null && description !== ''"
+          v-html="description"
+          class="text-2xs"
+        />
       </div>
     </template>
   </VTooltip>
