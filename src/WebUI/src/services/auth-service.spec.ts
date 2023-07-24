a4111791be22a7fd12a87c01ce83b6b534dbc919
Valeriy Sinevich <zohan33333@gmail.com>
2023-07-24 22:57:26 +0000

    spec - auth service

--- src/WebUI/src/services/auth-service.spec.ts
+++ src/WebUI/src/services/auth-service.spec.ts
@@ -1,16 +1,10 @@
-const mockSigninRedirect = vi.fn();
-const mockGetUser = vi.fn();
-const mockSignoutRedirect = vi.fn();
+import { type User } from 'oidc-client-ts';
 
-import {
-  extractToken,
-  parseJwt,
-  getUser,
-  login,
-  logout,
-  getToken,
-  getDecodedToken,
-} from './auth-service';
+const { mockedSigninRedirect, mockedGetUser, mockedSignoutRedirect } = vi.hoisted(() => ({
+  mockedSigninRedirect: vi.fn(),
+  mockedGetUser: vi.fn(),
+  mockedSignoutRedirect: vi.fn(),
+}));
 
 const JWT_TOKEN =
   'eyJhbGciOiJSUzI1NiIsImtpZCI6IkUyMUZDMEIzMUYwRkUxOEQwM0I2ODUzNzAxQkRFQUIyIiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2NjU5NDk4NjUsImV4cCI6MTY2NTk1MDQ2NSwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6ODAwMCIsImNsaWVudF9pZCI6ImNycGctd2ViLXVpIiwic3ViIjoiMiIsImF1dGhfdGltZSI6MTY2NTk0OTg1MiwiaWRwIjoibG9jYWwiLCJyb2xlIjoiVXNlciIsImp0aSI6IjY2N0RFMzZDOTg0RUFDRENBMDZBNUVDQkVGNTBDRDBEIiwic2lkIjoiOTAxRTFBNUNEQUVBRjMxMjRBMjZCODJDRDUzNjM2RTYiLCJpYXQiOjE2NjU5NDk4NTMsInNjb3BlIjpbIm9wZW5pZCIsInVzZXJfYXBpIiwib2ZmbGluZV9hY2Nlc3MiXSwiYW1yIjpbInB3ZCJdfQ.Js5ze2JNSew0m5UD86HyA62cMvPxnFJACa8IhQW59vzTABticqtdo8070soza-11JJyT_zHAI97SWSQRUoQ4w1pCdPmMsh5HyMueMx-OO_cFpkZg6PkpQlaSYB_Z_916k5nhCRVkNK7X4H2MByhkMd1rh0yFFGvYKVAnWKNYZFYL2y9VPv510b8RV0JyfjZvwLRN-cU2n7xVHkSzSE7WYz6X3D9l4MS-cOZRt3Y62EqjuHbBY3sJk-VIJCAuc0puGmms69_-9KV5cMLNfwoHOihYctSB7JVh-oSlXxuvkZHz_23eQnvsW5DLCgrOKnxMQBj44TRBwqHmrLl89UV3GQ';
@@ -33,12 +27,15 @@ const DECODED_TOKEN = {
 
 vi.mock('oidc-client-ts', () => ({
   UserManager: vi.fn().mockImplementation(() => ({
-    signinRedirect: mockSigninRedirect,
-    getUser: mockGetUser,
-    signoutRedirect: mockSignoutRedirect,
+    signinRedirect: mockedSigninRedirect,
+    getUser: mockedGetUser,
+    signoutRedirect: mockedSignoutRedirect,
   })),
   WebStorageStateStore: vi.fn(),
 }));
+
+import { extractToken, parseJwt, getUser, login, logout, getToken } from './auth-service';
+import { Platform } from '@/models/platform';
 
 describe('utils', () => {
   it('extractToken', () => {
@@ -46,7 +43,7 @@ describe('utils', () => {
       access_token: JWT_TOKEN,
     };
 
-    expect(extractToken(payload)).toEqual(JWT_TOKEN);
+    expect(extractToken(payload as User)).toEqual(JWT_TOKEN);
   });
 
   it('parseJwt', () => {
@@ -55,40 +52,30 @@ describe('utils', () => {
 });
 
 it('getUser', async () => {
-  mockGetUser.mockResolvedValueOnce({ foo: 'bar' });
+  mockedGetUser.mockResolvedValueOnce({ foo: 'bar' });
   expect(await getUser()).toEqual({ foo: 'bar' });
-  expect(mockGetUser).toHaveBeenCalled();
+  expect(mockedGetUser).toHaveBeenCalled();
 });
 
-it('login - TODO:', async () => {
-  await login();
-  expect(mockSigninRedirect).toHaveBeenCalled();
+it('login', async () => {
+  await login(Platform.Steam);
+  expect(mockedSigninRedirect).toHaveBeenCalledWith({
+    extraQueryParams: {
+      identity_provider: 'Steam',
+    },
+  });
 });
 
 it('logout', async () => {
   await logout();
-  expect(mockSignoutRedirect).toHaveBeenCalled();
+  expect(mockedSignoutRedirect).toHaveBeenCalled();
 });
 
 it('getToken', async () => {
-  mockGetUser.mockResolvedValueOnce({ access_token: 'access_token' });
+  mockedGetUser.mockResolvedValueOnce({ access_token: 'access_token' });
 
   const token = await getToken();
 
   expect(token).toEqual('access_token');
-  expect(mockGetUser).toHaveBeenCalled();
-});
-
-it('getDecodedToken', async () => {
-  mockGetUser.mockResolvedValueOnce({ access_token: JWT_TOKEN });
-
-  const decodedToken = await getDecodedToken();
-
-  expect(decodedToken).toEqual({
-    userId: parseInt(DECODED_TOKEN.sub, 10),
-    userRole: DECODED_TOKEN.role,
-    expiration: new Date(DECODED_TOKEN.exp * 1000),
-    issuedAt: new Date(DECODED_TOKEN.iat * 1000),
-  });
-  expect(mockGetUser).toHaveBeenCalled();
+  expect(mockedGetUser).toHaveBeenCalled();
 });
