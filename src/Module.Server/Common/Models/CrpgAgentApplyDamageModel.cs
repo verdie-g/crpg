3ef360c1ce7292ad46b0e990f1891ec8fdd25c99
namidaka <47090987+namidaka@users.noreply.github.com>
2023-07-30 17:37:20 +0200

    mod: lower bonus versus shields (#1214)

--- src/Module.Server/Common/Models/CrpgAgentApplyDamageModel.cs
+++ src/Module.Server/Common/Models/CrpgAgentApplyDamageModel.cs
@@ -37,6 +37,12 @@ internal class CrpgAgentApplyDamageModel : MultiplayerAgentApplyDamageModel
             WeaponClass.OneHandedPolearm,
             WeaponClass.TwoHandedPolearm,
         };
+        List<WeaponClass> swordClass = new()
+        {
+            WeaponClass.Dagger,
+            WeaponClass.OneHandedSword,
+            WeaponClass.TwoHandedSword,
+        };
         float finalDamage = base.CalculateDamage(attackInformation, collisionData, weapon, baseDamage);
         if (weapon.IsEmpty)
         {
@@ -65,10 +71,11 @@ internal class CrpgAgentApplyDamageModel : MultiplayerAgentApplyDamageModel
                 if (weapon.CurrentUsageItem.WeaponFlags.HasAnyFlag(WeaponFlags.BonusAgainstShield))
                 {
                     // this bonus is on top of the native x2 in MissionCombatMechanicsHelper
-                    // so the final bonus is 3.5. We do this instead of nerfing the impact of shield skill so shield can stay virtually unbreakable against sword.
+                    // so the final bonus is 3.5 for axes and 3 for swords. We do this instead of nerfing the impact of shield skill so shield can stay virtually unbreakable against sword.
                     // it is the same logic as arrows not dealing a lot of damage to horse but spears dealing extra damage to horses
                     // As we want archer to fear cavs and cavs to fear spears, we want swords to fear shielders and shielders to fear axes.
-                    finalDamage *= 2f;
+
+                    finalDamage *= swordClass.Contains(weapon.CurrentUsageItem.WeaponClass) ? 1.5f : 1.75f;
                 }
             }
         }
