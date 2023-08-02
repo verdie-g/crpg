4add2eec2ff86e4bf31b29d408114e963f85139c
Harry Gardner <93069911+Muparadzi@users.noreply.github.com>
2023-08-02 01:22:59 +0100

    mod: add dtv viscount attacked message (#1218)

---
+++ src/Module.Server/Modes/Dtv/CrpgDtvViscountUnderAttackMessage.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.Network.Messages;
+
+namespace Crpg.Module.Modes.Dtv;
+
+[DefineGameNetworkMessageTypeForMod(GameNetworkMessageSendType.FromServer)]
+internal sealed class CrpgDtvViscountUnderAttackMessage : GameNetworkMessage
+{
+    public Agent? Attacker { get; set; }
+    protected override void OnWrite()
+    {
+        WriteAgentReferenceToPacket(Attacker);
+    }
+
+    protected override bool OnRead()
+    {
+        bool bufferReadValid = true;
+        Attacker = ReadAgentReferenceFromPacket(ref bufferReadValid);
+        return bufferReadValid;
+    }
+
+    protected override MultiplayerMessageFilter OnGetLogFilter()
+    {
+        return MultiplayerMessageFilter.GameMode;
+    }
+
+    protected override string OnGetLogFormat()
+    {
+        return "cRPG DTV Viscount Under Attack";
+    }
+}
